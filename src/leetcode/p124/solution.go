5ccd153e0479c56e5bcda0d7f68098b1dd195129
WangSenyuan <brucewang@maycur.com>
2016-08-18 10:35:27 +0800

    aaaa

---
+++ src/leetcode/p124/solution.go
@@ -0,0 +1,39 @@
+package main
+
+import "math"
+
+/**
+ * Definition for a binary tree node.
+ */
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func maxPathSum(root *TreeNode) int {
+	result := math.MinInt32
+
+	var process func(*TreeNode) int
+
+	process = func(root *TreeNode) int {
+		if root == nil {
+			return 0
+		}
+		left := max(0, process(root.Left))
+		right := max(0, process(root.Right))
+		result = max(result, left+right+root.Val)
+		return max(left, right) + root.Val
+	}
+
+	process(root)
+
+	return result
+}
+
+func max(a, b int) int {
+	if a >= b {
+		return a
+	}
+	return b
+}
---
+++ src/leetcode/p128/solution.go
@@ -0,0 +1,23 @@
+package main
+
+func longestConsecutive(nums []int) int {
+	m := make(map[int]int)
+
+	res := 0
+	for _, num := range nums {
+		if _, has := m[num]; has {
+			continue
+		}
+
+		left, right := m[num-1], m[num+1]
+		m[num] = left + right + 1
+		m[num-left] = left + right + 1
+		m[num+right] = left + right + 1
+
+		if left+right+1 > res {
+			res = left + right + 1
+		}
+	}
+
+	return res
+}

e1b58a5923c8a4e7d02878844dbf93ce9a66040b
WangSenyuan <brucewang@maycur.com>
2016-11-07 19:49:29 +0800

    aaa

--- src/leetcode/p446/solution.go
+++ src/leetcode/p446/solution.go
@@ -7,19 +7,23 @@ func main() {
 }
 
 func numberOfArithmeticSlices(A []int) int {
+	//DP[i][d] = the number of arithmetic subsequences whose last but one number is A[i], difference is d.
 	dp := make([]map[int]int, len(A))
-	for i := range A {
+	nums := make(map[int]bool)
+	for i, a := range A {
 		dp[i] = make(map[int]int)
+		nums[a] = true
 	}
 	ans := 0
 	for i := 1; i < len(A); i++ {
-		for j := 0; j < i; j++ {
+		for j := i - 1; j >= 0; j-- {
 			dist := A[i] - A[j]
-			s := dp[j][dist] + 1
-			dp[i][dist] += s
+			s := dp[j][dist]
 			ans += s
+			if nums[A[i]+dist] {
+				dp[i][dist] += 1 + s
+			}
 		}
-		ans -= i
 	}
 
 	return ans
--- src/leetcode/p447/solution.go
+++ src/leetcode/p447/solution.go
@@ -1,1 +1,39 @@
 package main
+
+import "fmt"
+
+func main() {
+	points := [][]int{[]int{0, 0}, []int{1, 0}, []int{2, 0}}
+	fmt.Println(numberOfBoomerangs(points))
+}
+
+func numberOfBoomerangs(points [][]int) int {
+	if len(points) < 3 {
+		return 0
+	}
+
+	ans := 0
+	for i, a := range points {
+		dist := make(map[int64]int)
+		for j, b := range points {
+			if i == j {
+				continue
+			}
+
+			d := distance(a, b)
+			if dist[d] > 0 {
+				ans += 2 * dist[d]
+			}
+			dist[d]++
+		}
+	}
+
+	return ans
+}
+
+func distance(a, b []int) int64 {
+	x := int64(b[0] - a[0])
+	y := int64(b[1] - a[1])
+
+	return x*x + y*y
+}

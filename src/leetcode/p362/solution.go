888668a7e5b63405528366ba5c98869e2239a680
WangSenyuan <brucewang@maycur.com>
2016-11-29 15:34:20 +0800

    code improve

--- src/leetcode/p362/solution.go
+++ src/leetcode/p362/solution.go
@@ -1,9 +1,6 @@
 package main
 
-import (
-	"fmt"
-	"sort"
-)
+import "fmt"
 
 func main() {
 	hc := Constructor()
@@ -16,55 +13,28 @@ func main() {
 
 //HitCounter is used to record the hits number in the last 5 mins
 type HitCounter struct {
-	ts  []int
-	cnt map[int]int
+	ts []int
 }
 
 /** Initialize your data structure here. */
 func Constructor() HitCounter {
-	return HitCounter{make([]int, 0, 100), make(map[int]int)}
+	return HitCounter{make([]int, 0, 100)}
 }
 
 /** Record a hit.
   @param timestamp - The current timestamp (in seconds granularity). */
 func (this *HitCounter) Hit(timestamp int) {
-	if _, found := this.cnt[timestamp]; !found {
-		recordTimestamp(this, timestamp)
-	}
-	this.cnt[timestamp]++
-}
-
-func recordTimestamp(hc *HitCounter, ts int) {
-	if len(hc.ts)+1 == cap(hc.ts) {
-		tmp := make([]int, len(hc.ts), 2*cap(hc.ts))
-		copy(tmp, hc.ts)
-		hc.ts = tmp
-	}
-	hc.ts = append(hc.ts, ts)
+	this.ts = append(this.ts, timestamp)
 }
 
 /** Return the number of hits in the past 5 minutes.
   @param timestamp - The current timestamp (in seconds granularity). */
 func (this *HitCounter) Gethits(timestamp int) int {
-	expired, left := sliceWindow(this, timestamp)
-	this.ts = left
-	for _, ts := range expired {
-		delete(this.cnt, ts)
+	i := 0
+	for i < len(this.ts) && this.ts[i] <= timestamp-300 {
+		i++
 	}
+	this.ts = this.ts[i:]
 
-	cnt := 0
-	for _, ts := range left {
-		if ts > timestamp {
-			break
-		}
-		cnt += this.cnt[ts]
-	}
-	return cnt
-}
-
-func sliceWindow(hc *HitCounter, ts int) ([]int, []int) {
-	lo := sort.Search(len(hc.ts), func(i int) bool {
-		return hc.ts[i] > ts-300
-	})
-	return hc.ts[:lo], hc.ts[lo:]
+	return len(this.ts)
 }

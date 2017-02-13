993ee42b86d5caa6f9b7120cdec3a47ef5959908
WangSenyuan <brucewang@maycur.com>
2017-02-13 20:28:31 +0800

    aaa

--- src/leetcode/p493/solution.go
+++ src/leetcode/p493/solution.go
@@ -5,63 +5,69 @@ import (
 )
 
 func main() {
-	nums := []int{2, 4, 3, 5, 1}
+	//nums := []int{2, 4, 3, 5, 1}
 	//nums := []int{1, 3, 2, 3, 1}
+	nums := []int{-2, -2}
 	fmt.Println(reversePairs(nums))
 }
 
 func reversePairs(nums []int) int {
 	var res int
-	var root *Node
-	for i := len(nums) - 1; i >= 0; i-- {
-		num := nums[i]
-		res += search(root, float32(num)/2.0)
-		root = build(root, float32(num))
+	help := make([]int, len(nums))
+	var sort func(left, right int)
+
+	sort = func(left, right int) {
+		if left+1 >= right {
+			return
+		}
+
+		mid := left + (right-left)/2
+		sort(left, mid)
+		sort(mid, right)
+
+		i, j, k, u := left, mid, left, left
+
+		for i < mid && j < right {
+			if nums[i] < nums[j] {
+				help[k] = nums[i]
+				i++
+			} else {
+				help[k] = nums[j]
+
+				for u < mid && nums[u] <= 2*nums[j] {
+					u++
+				}
+				res += mid - u
+
+				j++
+
+			}
+
+			k++
+		}
+
+		for i < mid {
+			help[k] = nums[i]
+			k++
+			i++
+		}
+		for j < right {
+			help[k] = nums[j]
+			k++
+
+			for u < mid && nums[u] <= 2*nums[j] {
+				u++
+			}
+			res += mid - u
+
+
+			j++
+		}
+
+		copy(nums[left:right], help[left:right])
 	}
+
+	sort(0, len(nums))
 
 	return res
-}
-
-type Node struct {
-	val         float32
-	eq, lt      int
-	left, right *Node
-}
-
-func search(root *Node, val float32) int {
-
-	var find func(root *Node, cnt int) int
-	find = func(root *Node, cnt int) int {
-		if root == nil {
-			return cnt
-		}
-		if val == root.val {
-			return root.lt + cnt;
-		}
-
-		if val < root.val {
-			return find(root.left, cnt)
-		}
-
-		return find(root.right, cnt+root.eq+root.lt)
-	}
-
-	return find(root, 0)
-}
-
-func build(root *Node, val float32) *Node {
-	if root == nil {
-		return &Node{val, 1, 0, nil, nil}
-	}
-
-	if root.val == val {
-		root.eq++
-	} else if root.val < val {
-		root.right = build(root.right, val)
-	} else {
-		root.left = build(root.left, val)
-		root.lt++
-	}
-	return root
-
 }

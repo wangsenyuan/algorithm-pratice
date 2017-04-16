b3d427ecbfcd2184dc39a6509397fd9e61b1c24a
WangSenyuan <brucewang@maycur.com>
2017-04-16 18:12:49 +0800

    another state machine based solution for problem 552

--- src/leetcode/p552/solution.go
+++ src/leetcode/p552/solution.go
@@ -1,6 +1,16 @@
 package main
 
-func checkRecord(n int) int {
+import "fmt"
+
+func main() {
+	for i := 1; i < 10; i++ {
+		fmt.Printf("%d => %d\n", i, checkRecord(i))
+	}
+}
+
+const MOD = 1000000007
+
+func checkRecord1(n int) int {
 	//no A: f(n) = f(n- 1) + f(n - 2) + f(n - 3)
 	//has one A: change any one of letters from the above to A n * f(n)
 	//f(1) = 1
@@ -14,8 +24,6 @@ func checkRecord(n int) int {
 	if n == 3 {
 		return 19
 	}
-
-	MOD := int64(1000000007)
 
 	f := make([]int64, n+1)
 	f[1] = 2 //P & L
@@ -40,4 +48,28 @@ func checkRecord(n int) int {
 	}
 
 	return int(ans)
+}
+
+func checkRecord(n int) int {
+	a0l0 := int64(1)
+	var a0l1, a0l2, a1l0, a1l1, a1l2 int64
+
+	for i := 0; i < n; i++ {
+		new_a0l0 := (a0l0 + a0l1 + a0l2) % MOD
+		new_a0l1 := a0l0
+		new_a0l2 := a0l1
+		new_a1l0 := (a1l0 + a0l0 + a0l1 + a0l2 + a1l1 + a1l2) % MOD
+		new_a1l1 := a1l0
+		new_a1l2 := a1l1
+
+		a0l0 = new_a0l0
+		a0l1 = new_a0l1
+		a0l2 = new_a0l2
+		a1l0 = new_a1l0
+		a1l1 = new_a1l1
+		a1l2 = new_a1l2
+	}
+
+	res := (a0l0 + a0l1 + a0l2 + a1l0 + a1l1 + a1l2) % MOD
+	return int(res)
 }

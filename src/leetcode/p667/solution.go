ad544b07797ec392d08862b6ccf08766b52cb4db
WangSenyuan <brucewang@maycur.com>
2017-08-27 17:04:02 +0800

    aaa

--- src/leetcode/p667/solution.go
+++ src/leetcode/p667/solution.go
@@ -3,31 +3,50 @@ package main
 import "fmt"
 
 func constructArray(n int, k int) []int {
-	arr := make([]int, n)
+	arr := make([]int, 0, n)
 
-	m := k
+	i, j := 1, n
 
-	for i := m; i < n; i++ {
-		arr[i] = i + 1
-	}
-
-	x := k
-	for i := m; i > 0; i-- {
-		a := arr[i]
-		b := a + x
-		c := a - x
-		if c > 0 {
-			arr[i-1] = c
+	for i <= j {
+		if k%2 == 1 {
+			arr = append(arr, i)
+			i++
 		} else {
-			arr[i-1] = b
+			arr = append(arr, j)
+			j--
 		}
-		x--
+		if k > 1 {
+			k--
+		}
 	}
+
 	return arr
+}
+
+func isCorrect(input []int, k int) bool {
+	set := make(map[int]bool)
+
+	for i := 0; i < len(input)-1; i++ {
+		set[abs(input[i+1]-input[1])] = true
+	}
+
+	fmt.Println(len(set))
+	return len(set) == k
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
 }
 
 func main() {
 	fmt.Println(constructArray(6, 5))
+	fmt.Println(constructArray(6, 4))
 	fmt.Println(constructArray(6, 3))
+	fmt.Println(constructArray(6, 2))
+	fmt.Println(constructArray(6, 1))
 	fmt.Println(constructArray(9999, 9998))
+	fmt.Println(isCorrect(constructArray(9999, 9998), 9998))
 }

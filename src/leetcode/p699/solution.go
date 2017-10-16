2bfa92dbd97d0f4691e89ec1dd9be020a483c181
wangsenyuan <wang.senyuan@gmail.com>
2017-10-16 23:10:55 +0800

    aaa

--- src/leetcode/p699/solution.go
+++ src/leetcode/p699/solution.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 type interval struct {
 	start  int
@@ -8,7 +11,7 @@ type interval struct {
 	height int
 }
 
-func fallingSquares(positions [][]int) []int {
+func fallingSquares1(positions [][]int) []int {
 	n := len(positions)
 	res := make([]int, n)
 	intervals := make([]interval, 0, n)
@@ -37,6 +40,136 @@ func fallingSquares(positions [][]int) []int {
 	}
 
 	return res
+}
+
+type SegmentTree struct {
+	n    int
+	h    int
+	tree []int
+	lazy []int
+}
+
+func InitSegmentTree(n int) *SegmentTree {
+	h := 1
+	for 1<<uint(h) < n {
+		h++
+	}
+	return &SegmentTree{n, h, make([]int, 2*n), make([]int, n)}
+}
+
+func (st *SegmentTree) apply(i, v int) {
+	st.tree[i] = max(st.tree[i], v)
+	if i < st.n {
+		st.lazy[i] = max(st.lazy[i], v)
+	}
+}
+
+func (st *SegmentTree) pull(i int) {
+	for i > 1 {
+		i >>= 1
+		st.tree[i] = max(st.tree[2*i], st.tree[2*i+1])
+		st.tree[i] = max(st.tree[i], st.lazy[i])
+	}
+}
+
+func (st *SegmentTree) Update(left int, right int, h int) {
+	left += st.n
+	right += st.n
+	i, j := left, right
+	for left <= right {
+		if left&1 == 1 {
+			st.apply(left, h)
+			left++
+		}
+		if right&1 == 0 {
+			st.apply(right, h)
+			right--
+		}
+		left >>= 1
+		right >>= 1
+	}
+	st.pull(i)
+	st.pull(j)
+}
+
+func (st *SegmentTree) Query(left int, right int) int {
+	left += st.n
+	right += st.n
+	ans := 0
+	st.push(left)
+	st.push(right)
+	for left <= right {
+		if left&1 == 1 {
+			ans = max(ans, st.tree[left])
+			left++
+		}
+		if right&1 == 0 {
+			ans = max(ans, st.tree[right])
+			right--
+		}
+		left >>= 1
+		right >>= 1
+	}
+	return ans
+}
+
+func (st *SegmentTree) push(i int) {
+	for j := st.h; j > 0; j-- {
+		k := i >> uint(j)
+		if st.lazy[k] > 0 {
+			st.apply(k*2, st.lazy[k])
+			st.apply(k*2+1, st.lazy[k])
+			st.lazy[k] = 0
+		}
+	}
+
+}
+
+func max(a, b int) int {
+	if a >= b {
+		return a
+	}
+	return b
+}
+
+func fallingSquares(positions [][]int) []int {
+	set := make(map[int]bool)
+
+	for _, pos := range positions {
+		set[pos[0]] = true
+		set[pos[0]+pos[1]-1] = true
+	}
+	n := len(set)
+
+	arr := make([]int, n)
+
+	idx := 0
+	for coord, _ := range set {
+		arr[idx] = coord
+		idx++
+	}
+
+	sort.Ints(arr)
+
+	index := make(map[int]int)
+
+	for i, coord := range arr {
+		index[coord] = i
+	}
+
+	st := InitSegmentTree(n)
+	ans := make([]int, len(positions))
+	best := 0
+	for i, pos := range positions {
+		left := index[pos[0]]
+		right := index[pos[0]+pos[1]-1]
+		height := st.Query(left, right) + pos[1]
+		st.Update(left, right, height)
+		best = max(best, height)
+		ans[i] = best
+	}
+
+	return ans
 }
 
 func main() {

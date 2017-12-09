a5f18587e6a2dc7a6ef88b79238ae37fc8637fa6
WangSenyuan <brucewang@maycur.com>
2017-12-09 14:11:32 +0800

    aaa

---
+++ src/codechef/easy/lokbil/solution.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func readInt(bytes []byte, from int, val *int) int {
+	i := from
+	tmp := 0
+	for i < len(bytes) && bytes[i] != ' ' {
+		tmp = tmp*10 + int(bytes[i]-'0')
+		i++
+	}
+	*val = tmp
+	return i
+}
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	var g int
+	scanner.Scan()
+	readInt(scanner.Bytes(), 0, &g)
+
+	for i := 0; i < g; i++ {
+		var n int
+		scanner.Scan()
+		readInt(scanner.Bytes(), 0, &n)
+		friends := make([][]int, n)
+
+		for j := 0; j < n; j++ {
+			friends[j] = make([]int, n)
+			scanner.Scan()
+			bs := scanner.Bytes()
+			pos := 0
+			for pos < len(bs) {
+				var x int
+				pos = readInt(bs, pos, &x) + 1
+				friends[j][x-1] = 1
+			}
+		}
+		a, d := solve(n, friends)
+		fmt.Printf("%d %f\n", a+1, d)
+	}
+}
+
+func solve(n int, friends [][]int) (int, float64) {
+
+	bfs := func(x int) int {
+		ds := make([]int, n)
+		for i := 0; i < n; i++ {
+			ds[i] = -1
+		}
+		ds[x] = 0
+		que := make([]int, n)
+		head, tail := 0, 0
+		que[tail] = x
+		tail++
+		var total int
+		for head < tail {
+			cur := que[head]
+			head++
+			for i := 0; i < n; i++ {
+				if friends[cur][i] == 1 && ds[i] == -1 {
+					ds[i] = ds[cur] + 1
+					total += ds[i]
+					que[tail] = i
+					tail++
+				}
+			}
+		}
+		return total
+	}
+
+	x, min := -1, 0
+	for i := 0; i < n; i++ {
+		tmp := bfs(i)
+		if x == -1 || tmp < min {
+			x, min = i, tmp
+		}
+	}
+
+	return x, float64(min) / float64(n)
+}

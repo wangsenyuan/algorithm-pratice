468d3fd2b8f73b33ee74f361d1ef2503fb2ce32a
WangSenyuan <brucewang@maycur.com>
2017-12-31 18:12:02 +0800

    aaa

--- src/leetcode/set700/p756/solution.go
+++ src/leetcode/set700/p756/solution.go
@@ -14,6 +14,78 @@ func pyramidTransition(bottom string, allowed []string) bool {
 		a, b, c := int(s[0]-'A'), int(s[1]-'A'), int(s[2]-'A')
 		allow[a][b][c] = true
 	}
+
+	var generate func(bottom string) []string
+
+	generate = func(bottom string) []string {
+		res := make([]string, 0, 10)
+
+		var f func(i int, s string)
+
+		f = func(i int, s string) {
+			if i == len(bottom)-1 {
+				res = append(res, s)
+				return
+			}
+			a, b := int(bottom[i]-'A'), int(bottom[i+1]-'A')
+			for x := 0; x < 26; x++ {
+				if allow[a][b][x] {
+					f(i+1, s+string(byte(x+'A')))
+				}
+			}
+		}
+
+		f(0, "")
+
+		return res
+	}
+
+	cache := make(map[string]int)
+
+	var process func(bottom string) bool
+
+	process = func(bottom string) bool {
+		if len(bottom) == 1 {
+			return true
+		}
+
+		if cache[bottom] == 1 {
+			return true
+		}
+		if cache[bottom] == -1 {
+			return false
+		}
+		ans := -1
+		strs := generate(bottom)
+		if len(strs) > 0 {
+			for _, str := range strs {
+				if process(str) {
+					ans = 1
+					break
+				}
+			}
+		}
+		cache[bottom] = ans
+		return ans > 0
+	}
+
+	return process(bottom)
+}
+
+func pyramidTransition1(bottom string, allowed []string) bool {
+	allow := make([][][]bool, 26)
+	for x := 0; x < 26; x++ {
+		allow[x] = make([][]bool, 26)
+		for y := 0; y < 26; y++ {
+			allow[x][y] = make([]bool, 26)
+		}
+	}
+
+	for i := 0; i < len(allowed); i++ {
+		s := allowed[i]
+		a, b, c := int(s[0]-'A'), int(s[1]-'A'), int(s[2]-'A')
+		allow[a][b][c] = true
+	}
 	n := len(bottom)
 	f := make([][]bool, n)
 	g := make([][]bool, n)
--- src/leetcode/set700/p756/solution_test.go
+++ src/leetcode/set700/p756/solution_test.go
@@ -26,3 +26,19 @@ func TestSample2(t *testing.T) {
 	expect := false
 	runSample(t, bottom, allowed, expect)
 }
+
+func TestSample3(t *testing.T) {
+	bottom := "ABCD"
+	allowed := []string{
+		"BCE", "BCF", "ABA", "CDA", "AEG", "FAG", "GGG",
+	}
+	expect := false
+	runSample(t, bottom, allowed, expect)
+}
+
+func TestSample4(t *testing.T) {
+	bottom := "DACCDGDDCFCGCFAGFADF"
+	allowed := []string{"BGF", "AGE", "AGC", "AGA", "CCE", "CCD", "EGA", "CCF", "CCA", "DCD", "DCA", "FGE", "FGA", "FGB", "BFB", "BFG", "BFD", "ECC", "DBA", "FAF", "DBF", "FDA", "FDC", "FDE", "BEA", "BEE", "AEA", "AEC", "AED", "EEE", "DEA", "DEC", "EEA", "CCG", "EEC", "DEG", "CEE", "CED", "CEC", "CEA", "GEC", "GEA", "GEF", "GEE", "BDE", "BDD", "GCE", "AFC", "DDC", "DDB", "EFB", "EFE", "DDE", "DDD", "CBC", "CBE", "ACB", "ACE", "BCD", "BCE", "BCA", "BCB", "BCC", "DGB", "ECF", "DGF", "ECB", "ECA", "CGD", "CGF", "FCE", "FEF", "BBF", "BBD", "ADG", "ADD", "ADA", "DFD", "DFC", "CDE", "CDF", "CDG", "EDF", "EDG", "EDD", "FBA", "FBG", "FBF", "GDF", "AAE", "AAD", "AAC", "BAG", "BAB", "BAC", "BAA", "CAF", "CAD", "DAD", "DAB", "DED", "EAD", "EAG", "EAF", "FAC", "GAD", "GAC", "GAB", "ABA", "ABF", "EBD", "EBF", "EBA", "EBB", "EBC", "CFF", "CFE", "CFC", "GFC", "GFA", "GFG", "GFD"}
+	expect := true
+	runSample(t, bottom, allowed, expect)
+}

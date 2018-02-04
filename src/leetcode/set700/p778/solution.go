0842094cd3a46571a1b032cf0ddb1e2229335d6f
WangSenyuan <brucewang@maycur.com>
2018-02-04 20:29:14 +0800

    aaa

--- src/leetcode/set700/p778/solution.go
+++ src/leetcode/set700/p778/solution.go
@@ -5,16 +5,13 @@ var dd = []int{-1, 0, 1, 0, -1}
 func swimInWater(grid [][]int) int {
 	n := len(grid)
 	que := make([]int, n*n)
-	dist := make([]int, n*n)
+	checked := make([]int, n*n)
 
 	check := func(time int) bool {
 		if time < grid[0][0] {
 			return false
 		}
-		for i := 0; i < n*n; i++ {
-			dist[i] = -1
-		}
-		dist[0] = grid[0][0]
+		checked[0] = time
 		head, tail := 0, 0
 		que[tail] = 0
 		tail++
@@ -29,10 +26,10 @@ func swimInWater(grid [][]int) int {
 				r, c := v/n, v%n
 				for j := 0; j < 4; j++ {
 					x, y := r+dd[j], c+dd[j+1]
-					if x >= 0 && x < n && y >= 0 && y < n && grid[x][y] <= time && dist[x*n+y] == -1 {
+					if x >= 0 && x < n && y >= 0 && y < n && grid[x][y] <= time && checked[x*n+y] != time {
 						que[tail] = x*n + y
 						tail++
-						dist[x*n+y] = dist[v] + 1
+						checked[x*n+y] = time
 					}
 				}
 			}

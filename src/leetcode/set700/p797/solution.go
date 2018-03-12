2c5522a12a1689cde56769be2deca6e91601aa0b
WangSenyuan <brucewang@maycur.com>
2018-03-12 14:11:41 +0800

    code refactor

--- src/leetcode/set700/p797/solution.go
+++ src/leetcode/set700/p797/solution.go
@@ -5,21 +5,16 @@ func allPathsSourceTarget(graph [][]int) [][]int {
 
 	var dfs func(v int, m int, path []int)
 	res := make([][]int, 0, n)
-	visited := make([]bool, n)
 
 	dfs = func(v int, m int, path []int) {
 		if v == n-1 {
 			res = append(res, copySlice(path[:m]))
 			return
 		}
-		visited[v] = true
 		for _, w := range graph[v] {
-			if !visited[w] {
-				path[m] = w
-				dfs(w, m+1, path)
-			}
+			path[m] = w
+			dfs(w, m+1, path)
 		}
-		visited[v] = false
 	}
 
 	path := make([]int, n)

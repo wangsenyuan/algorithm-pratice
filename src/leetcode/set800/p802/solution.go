3ccbbd529010ff598f5e4473e13523b3c93f4e48
WangSenyuan <brucewang@maycur.com>
2018-03-18 13:57:33 +0800

    bb

--- src/leetcode/set800/p802/solution.go
+++ src/leetcode/set800/p802/solution.go
@@ -2,6 +2,58 @@ package p802
 
 func eventualSafeNodes(graph [][]int) []int {
 	n := len(graph)
+	degrees := make([]int, n)
+
+	rg := make([]map[int]bool, n)
+
+	for i := 0; i < n; i++ {
+		rg[i] = make(map[int]bool)
+	}
+
+	for i, row := range graph {
+		for _, j := range row {
+			degrees[i]++
+			rg[j][i] = true
+		}
+	}
+
+	que := make([]int, n)
+	head, tail := 0, 0
+	for i := 0; i < n; i++ {
+		if degrees[i] == 0 {
+			que[tail] = i
+			tail++
+		}
+	}
+	can := make([]bool, n)
+	for head < tail {
+		tt := tail
+		for head < tt {
+			v := que[head]
+			can[v] = true
+			head++
+			for w := range rg[v] {
+				degrees[w]--
+				if degrees[w] == 0 {
+					que[tail] = w
+					tail++
+				}
+			}
+		}
+	}
+
+	res := make([]int, 0, n)
+	for i := 0; i < n; i++ {
+		if can[i] {
+			res = append(res, i)
+		}
+	}
+
+	return res
+}
+
+func eventualSafeNodes1(graph [][]int) []int {
+	n := len(graph)
 	visited := make([]int, n)
 	path := make([]bool, n)
 	res := make([]bool, n)

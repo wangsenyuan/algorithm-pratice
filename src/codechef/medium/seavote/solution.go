f7f2a0276071816c96a468d2a433a9bd2cd39bb5
WangSenyuan <brucewang@maycur.com>
2018-04-06 17:55:46 +0800

    aa

---
+++ src/codechef/medium/seavote/solution.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func readInt(bytes []byte, from int, val *int) int {
+	i := from
+	tmp := 0
+	for i < len(bytes) && bytes[i] != ' ' {
+		tmp = tmp*10 + int(bytes[i]-'0')
+		i++
+	}
+	*val = tmp
+	return i
+}
+
+func readNum(scanner *bufio.Scanner) (a int) {
+	scanner.Scan()
+	readInt(scanner.Bytes(), 0, &a)
+	return
+}
+
+func readTwoNums(scanner *bufio.Scanner) (a int, b int) {
+	scanner.Scan()
+	x := readInt(scanner.Bytes(), 0, &a)
+	readInt(scanner.Bytes(), x+1, &b)
+	return
+}
+
+func readNNums(scanner *bufio.Scanner, n int) []int {
+	res := make([]int, n)
+	x := -1
+	scanner.Scan()
+	for i := 0; i < n; i++ {
+		x = readInt(scanner.Bytes(), x+1, &res[i])
+	}
+	return res
+}
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	t := readNum(scanner)
+
+	for t > 0 {
+		n := readNum(scanner)
+		B := readNNums(scanner, n)
+
+		res := solve(n, B)
+
+		if res {
+			fmt.Println("YES")
+		} else {
+			fmt.Println("NO")
+		}
+
+		t--
+	}
+}
+
+func solve(n int, B []int) bool {
+	var s, m int
+
+	for i := 0; i < n; i++ {
+		s += B[i]
+		if B[i] > 0 {
+			m++
+		}
+	}
+
+	return s >= 100 && s < 100+m
+}

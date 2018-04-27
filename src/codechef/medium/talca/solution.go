05f247b8d9ea024eabe63c305befab605750aec4
WangSenyuan <brucewang@maycur.com>
2018-04-27 21:55:28 +0800

    aaa

--- src/codechef/medium/talca/solution.go
+++ src/codechef/medium/talca/solution.go
@@ -72,6 +72,127 @@ func solve(n int, pairs [][]int, queries [][]int) []int {
 		neighbors[v][u] = true
 	}
 
+	var eularTour func(p, u int, level int)
+
+	first := make([]int, n)
+	for i := 0; i < n; i++ {
+		first[i] = -1
+	}
+
+	var index int
+
+	nodes := make([]Node, 0, 3*n)
+
+	eularTour = func(p, u int, level int) {
+		index++
+		if first[u] < 0 {
+			first[u] = index - 1
+		}
+		nodes = append(nodes, Node{u, level})
+		for v := range neighbors[u] {
+			if v == p {
+				continue
+			}
+			eularTour(u, v, level+1)
+			level++
+			nodes = append(nodes, Node{u, level})
+		}
+	}
+
+	N := len(nodes)
+	trees := make([]int, 4*N)
+
+	var construct func(i int, left, right int)
+	construct = func(i int, left, right int) {
+		if left+1 == right {
+			trees[i] = left
+			return
+		}
+		mid := (left + right) / 2
+		construct(2*i+1, left, mid)
+		construct(2*i+2, mid, right)
+		a := nodes[trees[2*i+1]]
+		b := nodes[trees[2*i+2]]
+		if a.level < b.level {
+			trees[i] = trees[2*i+1]
+		} else {
+			trees[i] = trees[2*i+2]
+		}
+	}
+
+	construct(0, 0, N)
+
+	var query func(i int, s, e int, left, right int) int
+	query = func(i int, s, e int, left, right int) int {
+		if e <= left || s >= right {
+			return -1
+		}
+		if s >= left && e <= right {
+			return trees[i]
+		}
+
+		mid := (s + e) / 2
+		a := query(2*i+1, s, mid, left, right)
+		b := query(2*i+2, mid, e, left, right)
+		if a < 0 {
+			return b
+		}
+		if b < 0 {
+			return a
+		}
+		c, d := nodes[a], nodes[b]
+		if c.level < d.level {
+			return a
+		}
+		return b
+	}
+
+	lca := func(u, v int) int {
+		return query(0, 0, N, first[u], first[v])
+	}
+
+	res := make([]int, len(queries))
+
+	for i, query := range queries {
+		r, u, v := query[0]-1, query[1]-1, query[2]-1
+		uv := lca(u, v)
+		ru := lca(r, u)
+		rv := lca(r, v)
+		ruv := lca(r, uv)
+
+		if ruv != uv {
+			res[i] = uv + 1
+		} else {
+			if ru == uv && rv == uv {
+				res[i] = uv + 1
+			} else if rv == uv {
+				res[i] = ru + 1
+			} else {
+				res[i] = rv + 1
+			}
+		}
+	}
+
+	return res
+}
+
+type Node struct {
+	index, level int
+}
+
+func solve1(n int, pairs [][]int, queries [][]int) []int {
+	neighbors := make([]map[int]bool, n)
+
+	for i := 0; i < n; i++ {
+		neighbors[i] = make(map[int]bool)
+	}
+
+	for _, pair := range pairs {
+		u, v := pair[0]-1, pair[1]-1
+		neighbors[u][v] = true
+		neighbors[v][u] = true
+	}
+
 	level := make([]int, n)
 	parent := make([]int, n)
 

a52180813d71b092bc7ca986f9b45c797b8c193e
WangSenyuan <brucewang@maycur.com>
2018-05-10 18:32:24 +0800

    aaa

---
+++ src/codechef/medium/divgame/solution.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func readInt(bytes []byte, from int, val *int) int {
+	i := from
+	tmp := 0
+	for i < len(bytes) && bytes[i] != ' ' {
+		tmp = tmp*10 + int(bytes[i]-'0')
+		i++
+	}
+	*val = tmp
+	return i
+}
+
+func readNum(scanner *bufio.Scanner) (a int) {
+	scanner.Scan()
+	readInt(scanner.Bytes(), 0, &a)
+	return
+}
+
+func readTwoNums(scanner *bufio.Scanner) (a int, b int) {
+	scanner.Scan()
+	x := readInt(scanner.Bytes(), 0, &a)
+	readInt(scanner.Bytes(), x+1, &b)
+	return
+}
+
+func readNNums(scanner *bufio.Scanner, n int) []int {
+	res := make([]int, n)
+	x := -1
+	scanner.Scan()
+	for i := 0; i < n; i++ {
+		x = readInt(scanner.Bytes(), x+1, &res[i])
+	}
+	return res
+}
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	t := readNum(scanner)
+
+	for t > 0 {
+		n := readNum(scanner)
+		if !solve(n) {
+			fmt.Println("Mike")
+		} else {
+			fmt.Println("Tom")
+		}
+		t--
+	}
+}
+
+func solve(n int) bool {
+	return n == 16 || n == 34 || n == 289 || (n != 2 && n != 17 && prime(n))
+}
+
+func prime(n int) bool {
+	for x := 2; x*x <= n; x++ {
+		if n%x == 0 {
+			return false
+		}
+	}
+	return true
+}

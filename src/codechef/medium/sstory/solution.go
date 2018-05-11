771fac796706cdae64a6b13199b032be03e5cea2
WangSenyuan <brucewang@maycur.com>
2018-05-11 19:37:09 +0800

    aaa

---
+++ src/codechef/medium/sstory/solution.go
@@ -0,0 +1,193 @@
+package main
+
+import (
+	"sort"
+	"bufio"
+	"os"
+	"fmt"
+)
+
+func readInt(bytes []byte, from int, val *int) int {
+	i := from
+	tmp := 0
+	for i < len(bytes) && bytes[i] != ' ' {
+		tmp = tmp*10 + int(bytes[i]-'0')
+		i++
+	}
+	*val = tmp
+	return i
+}
+
+func readNum(scanner *bufio.Scanner) (a int) {
+	scanner.Scan()
+	readInt(scanner.Bytes(), 0, &a)
+	return
+}
+
+func readTwoNums(scanner *bufio.Scanner) (a int, b int) {
+	scanner.Scan()
+	x := readInt(scanner.Bytes(), 0, &a)
+	readInt(scanner.Bytes(), x+1, &b)
+	return
+}
+
+func readNNums(scanner *bufio.Scanner, n int) []int {
+	res := make([]int, n)
+	x := -1
+	scanner.Scan()
+	for i := 0; i < n; i++ {
+		x = readInt(scanner.Bytes(), x+1, &res[i])
+	}
+	return res
+}
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	s1 := scanner.Bytes()
+	scanner.Scan()
+	s2 := scanner.Bytes()
+	length, start := solve(s1, s2)
+	if length == 0 {
+		fmt.Println(0)
+	} else {
+		fmt.Println(string(s2[start : start+length]))
+	}
+}
+
+func solve(s1, s2 []byte) (length, start int) {
+	var mod int64 = 34369934
+	var magic int64 = 9584612342
+
+	check := func(l int) int {
+		if l == 0 {
+			return -1
+		}
+
+		var hash int64
+		var mul int64 = 1
+		for i := 0; i < l; i++ {
+			hash = (hash*magic + int64(s1[i])) % mod
+			mul = (mul * magic) % mod
+		}
+
+		mul = (mod - mul) % mod
+		hs := make(map[int64]bool)
+		hs[hash] = true
+
+		for i := l; i < len(s1); i++ {
+			hash = (hash*magic + int64(s1[i]) + mul*int64(s1[i-l])) % mod
+			hs[hash] = true
+		}
+		hash = 0
+
+		for i := 0; i < l; i++ {
+			hash = (hash*magic + int64(s2[i])) % mod
+		}
+		if hs[hash] {
+			return 0
+		}
+		for i := l; i < len(s2); i++ {
+			hash = (hash*magic + int64(s2[i]) + mul*int64(s2[i-l])) % mod
+			if hs[hash] {
+				return i - l
+			}
+		}
+		return -1
+	}
+
+	left, right := 0, len(s2)
+
+	for left < right {
+		mid := (left + right) / 2
+		res := check(mid)
+		if res >= 0 {
+			left = mid
+		} else {
+			right = mid - 1
+		}
+	}
+
+	start = check(left)
+
+	if start < 0 {
+		return
+	}
+
+	length = left
+	return
+}
+
+func solve1(s1, s2 []byte) (length int, start int) {
+	items := make(Items, len(s1)+len(s2))
+
+	for i := 0; i < len(s1)+len(s2); i++ {
+		if i < len(s1) {
+			items[i] = Item{s1, i, 1}
+		} else {
+			items[i] = Item{s2, i - len(s1), 2}
+		}
+	}
+
+	sort.Sort(items)
+
+	for i := len(items) - 2; i >= 0; i-- {
+		a, b := items[i+1], items[i]
+		if a.src == b.src {
+			// same string
+			continue
+		}
+		var k int
+		for a.idx+k < len(a.s) && b.idx+k < len(b.s) && a.s[a.idx+k] == b.s[b.idx+k] {
+			k++
+		}
+
+		c := a
+		if b.src == 2 {
+			c = b
+		}
+
+		if k > length || (k == length && c.idx < start) {
+			length = k
+			start = c.idx
+		}
+	}
+
+	return
+}
+
+type Item struct {
+	s   []byte
+	idx int
+	src int
+}
+
+type Items []Item
+
+func (items Items) Len() int {
+	return len(items)
+}
+
+func (items Items) Less(i, j int) bool {
+	a, b := items[i].s, items[j].s
+	ai, bi := items[i].idx, items[j].idx
+
+	for ai < len(a) && bi < len(b) {
+		if a[ai] < b[bi] {
+			return true
+		} else if a[ai] > b[bi] {
+			return false
+		}
+		ai++
+		bi++
+	}
+
+	if bi < len(b) {
+		return true
+	}
+	return false
+}
+
+func (items Items) Swap(i, j int) {
+	items[i], items[j] = items[j], items[i]
+}
---
+++ src/codechef/medium/sstory/solution_test.go
@@ -0,0 +1,34 @@
+package main
+
+import "testing"
+
+func runSample(t *testing.T, s1, s2 string, expect string) {
+	length, begin := solve([]byte(s1), []byte(s2))
+
+	if len(expect) != length {
+		t.Errorf("Sample %s %s, expect empty string, but got length %d", s1, s2, length)
+	} else if len(expect) > 0 && string(s2[begin:begin+length]) != expect {
+		t.Errorf("Sample %s %s, expect %s, but got %s", s1, s2, expect, string(s2[begin:begin+length]))
+	}
+}
+
+func TestSample1(t *testing.T) {
+	s1 := "adsyufsfdsfdsf"
+	s2 := "fdyusgfdfyu"
+	expect := "fd"
+	runSample(t, s1, s2, expect)
+}
+
+func TestSample2(t *testing.T) {
+	s1 := "adsyufsfdsfdsf"
+	s2 := "bbbbbbbbb"
+	expect := ""
+	runSample(t, s1, s2, expect)
+}
+
+func TestSample3(t *testing.T) {
+	s1 := "abcdef"
+	s2 := "defabc"
+	expect := "def"
+	runSample(t, s1, s2, expect)
+}

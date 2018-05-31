e1f175f5f573cc70a909f9a4b8c479f8eaa6d5f7
WangSenyuan <brucewang@maycur.com>
2018-05-31 20:38:37 +0800

    aaa

---
+++ src/codechef/medium/buttons/solution.go
@@ -0,0 +1,164 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func readInt(bytes []byte, from int, val *int) int {
+	i := from
+	tmp := 0
+	for i < len(bytes) && bytes[i] != ' ' {
+		tmp = tmp*10 + int(bytes[i]-'0')
+		i++
+	}
+	*val = tmp
+	return i
+}
+
+func readNum(scanner *bufio.Scanner) (a int) {
+	scanner.Scan()
+	readInt(scanner.Bytes(), 0, &a)
+	return
+}
+
+func readTwoNums(scanner *bufio.Scanner) (a int, b int) {
+	scanner.Scan()
+	x := readInt(scanner.Bytes(), 0, &a)
+	readInt(scanner.Bytes(), x+1, &b)
+	return
+}
+
+func readNNums(scanner *bufio.Scanner, n int) []int {
+	res := make([]int, n)
+	x := -1
+	scanner.Scan()
+	for i := 0; i < n; i++ {
+		x = readInt(scanner.Bytes(), x+1, &res[i])
+	}
+	return res
+}
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	t := readNum(scanner)
+
+	for t > 0 {
+		n := readNum(scanner)
+		src := make([][]int, n)
+		for i := 0; i < n; i++ {
+			src[i] = readNNums(scanner, n)
+		}
+		dst := make([][]int, n)
+		for i := 0; i < n; i++ {
+			dst[i] = readNNums(scanner, n)
+		}
+		can, row, col := solve(n, src, dst)
+		if !can {
+			fmt.Println(-1)
+		} else if len(row) == 0 && len(col) == 0 {
+			fmt.Println(0)
+		} else {
+			fmt.Println(len(row))
+			for _, idx := range row {
+				fmt.Println(idx)
+			}
+			fmt.Println(len(col))
+			for _, idx := range col {
+				fmt.Println(idx)
+			}
+		}
+		t--
+	}
+}
+
+func solve(n int, src, dst [][]int) (bool, []int, []int) {
+
+	find := func(flippedCols []bool, rows []bool) int {
+		var ans int
+		for i := 1; i < n; i++ {
+			var rowFliped bool
+
+			for j := 0; j < n; j++ {
+				x := src[i][j]
+				if flippedCols[j] {
+					x = 1 - x
+				}
+				if rowFliped {
+					x = 1 - x
+				}
+				if x != dst[i][j] {
+					if rowFliped {
+						return -1
+					}
+					rowFliped = true
+				}
+			}
+			if rowFliped {
+				rows[i] = true
+				ans++
+			}
+		}
+
+		return ans
+	}
+
+	cols0 := make([]bool, n)
+	rows0 := make([]bool, n)
+	for j := 0; j < n; j++ {
+		cols0[j] = false
+		if src[0][j] != dst[0][j] {
+			cols0[j] = true
+		}
+	}
+	row0NotFlip := find(cols0, rows0)
+
+	rows1 := make([]bool, n)
+	cols1 := make([]bool, n)
+	for j := 0; j < n; j++ {
+		cols1[j] = false
+		if src[0][j] == dst[0][j] {
+			cols1[j] = true
+		}
+	}
+	rows1[0] = true
+	row0Flip := find(cols1, rows1)
+
+	if row0NotFlip < 0 && row0Flip < 0 {
+		return false, nil, nil
+	}
+	if row0NotFlip < 0 {
+		return true, compact(rows1), compact(cols1)
+	}
+
+	if row0Flip < 0 {
+		return true, compact(rows0), compact(cols0)
+	}
+
+	if row0Flip+1 <= row0NotFlip {
+		return true, compact(rows1), compact(cols1)
+	}
+
+	return true, compact(rows0), compact(cols0)
+}
+
+func compact(row []bool) []int {
+	res := make([]int, len(row))
+	var j int
+	for i := 0; i < len(row); i++ {
+		if row[i] {
+			res[j] = i
+			j++
+		}
+	}
+	return res[:j]
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
---
+++ src/codechef/medium/buttons/solution_test.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func runSample(t *testing.T, n int, src [][]int, dst [][]int, expect bool, row []int, col []int) {
+	res, a, b := solve(n, src, dst)
+
+	if res != expect || !reflect.DeepEqual(row, a) || !reflect.DeepEqual(col, b) {
+		t.Errorf("Sample %d %v %v, expect %t %v %v, but got %t %v %v", n, src, dst, expect, row, col, res, a, b)
+	}
+}
+
+func TestSample1(t *testing.T) {
+	n := 3
+	src := [][]int{
+		{0, 0, 0},
+		{1, 1, 0},
+		{1, 1, 0},
+	}
+	dst := [][]int{
+		{1, 1, 0},
+		{1, 1, 1},
+		{1, 1, 1},
+	}
+	row := []int{0}
+	col := []int{2}
+	runSample(t, n, src, dst, true, row, col)
+}

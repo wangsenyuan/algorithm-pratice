56dad1fff93406fecc6f373e2664942eb4797824
WangSenyuan <brucewang@maycur.com>
2018-06-24 13:23:25 +0800

    aaa

--- src/leetcode/set800/p858/solution.go
+++ src/leetcode/set800/p858/solution.go
@@ -1,10 +1,27 @@
 package p858
 
 func mirrorReflection(p int, q int) int {
-	for p&1 == 0 && q&1 == 0 {
-		p >>= 1
-		q >>= 1
+	f := lcm(p, q)
+
+	x, y := f/p, f/q
+
+	if y%2 == 0 {
+		return 2
 	}
 
-	return 1 - p%2 + q%2
+	if x%2 == 1 {
+		return 1
+	}
+
+	return 0
+}
+
+func lcm(a, b int) int {
+	x := a * b
+
+	for b > 0 {
+		a, b = b, a%b
+	}
+
+	return x / a
 }

34c41e0a14a75380d16415f1dc2b3f48f833224a
WangSenyuan <brucewang@maycur.com>
2018-07-19 22:18:11 +0800

    aaa

--- src/codejam/year2008/round3/d/solution.go
+++ src/codejam/year2008/round3/d/solution.go
@@ -3,10 +3,11 @@ package main
 import (
 	"fmt"
 	"os"
+	"sort"
 )
 
 func main() {
-	f, err := os.Open("./D-small-practice.in")
+	f, err := os.Open("./D-large-practice.in")
 	if err != nil {
 		panic(err)
 	}
@@ -17,14 +18,15 @@ func main() {
 	fmt.Fscanf(f, "%d", &tc)
 
 	for i := 1; i <= tc; i++ {
-		var H, W, R int
+		var H, W int64
+		var R int
 		fmt.Fscanf(f, "%d %d %d", &H, &W, &R)
-		rocks := make([][]int, R)
+		rocks := make([][]int64, R)
 		for j := 0; j < R; j++ {
-			rocks[j] = make([]int, 2)
+			rocks[j] = make([]int64, 2)
 			fmt.Fscanf(f, "%d %d", &rocks[j][0], &rocks[j][1])
 		}
-		res := solveSmall(H, W, R, rocks)
+		res := solve(H, W, R, rocks)
 		fmt.Printf("Case #%d: %d\n", i, res)
 	}
 }
@@ -51,25 +53,89 @@ func init() {
 }
 
 func solve(H, W int64, R int, rocks [][]int64) int {
+	var res int64
 
+	for state := 0; state < 1<<uint(R); state++ {
+		tmp := calculate(state, rocks, H, W)
+		cnt := countBits(state, R)
+		if cnt%2 == 0 {
+			res += tmp
+			if res >= MOD {
+				res -= MOD
+			}
+		} else {
+			res -= tmp
+			if res < 0 {
+				res += MOD
+			}
+		}
+	}
+
+	return int(res)
 }
 
-func calculate(r, c int64) int64 {
-	if (r+c)%3 != 2 {
+func countBits(state int, size int) int {
+	var cnt int
+	for i := 0; i < size; i++ {
+		if state&(1<<uint(i)) > 0 {
+			cnt++
+		}
+	}
+	return cnt
+}
+
+func calculate(state int, rocks [][]int64, H, W int64) int64 {
+	cnt := countBits(state, len(rocks))
+	list := make(Pairs, cnt+2)
+	list[0] = Pair{1, 1}
+	list[1] = Pair{H, W}
+	cnt = 2
+	for i := 0; i < len(rocks); i++ {
+		if state&(1<<uint(i)) > 0 {
+			list[cnt] = Pair{rocks[i][0], rocks[i][1]}
+			cnt++
+		}
+	}
+	sort.Sort(list)
+	res := int64(1)
+
+	for i := 0; i < cnt-1; i++ {
+		a, b := list[i], list[i+1]
+		// a.first <= b.first
+		if a.second > b.second {
+			return 0
+		}
+		res = (res * calculate1(b.first-a.first, b.second-a.second)) % MOD
+	}
+
+	return res
+}
+
+func calculate1(a, b int64) int64 {
+	x := (2*b - a) / 3
+	y := (2*a - b) / 3
+	if x+2*y != a || y+2*x != b || x < 0 || y < 0 {
 		return 0
 	}
-	rr := r - 1 - (r+c-2)/3
-	cc := c - 1 - (r+c-2)/3
 
+	return calculate2(x+y, x)
 }
 
-func C(n, k int64) int64 {
+func calculate2(n, k int64) int64 {
+	if n < k {
+		return 0
+	}
+	if k == 0 || n == k {
+		return 1
+	}
 	res := int64(1)
 
 	for n > 0 && k > 0 {
 		x := n % MOD
 		y := k % MOD
 		res = (res * choose(int(x), int(y))) % MOD
+		n /= MOD
+		k /= MOD
 	}
 	return res
 }
@@ -120,4 +186,22 @@ func solveSmall(H, W, R int, rocks [][]int) int {
 	}
 
 	return dp[H][W]
+}
+
+type Pair struct {
+	first, second int64
+}
+
+type Pairs []Pair
+
+func (ps Pairs) Len() int {
+	return len(ps)
+}
+
+func (ps Pairs) Less(i, j int) bool {
+	return ps[i].first < ps[j].first || (ps[i].first == ps[j].first && ps[i].second < ps[j].second)
+}
+
+func (ps Pairs) Swap(i, j int) {
+	ps[i], ps[j] = ps[j], ps[i]
 }
--- src/codejam/year2008/round3/d/solution_test.go
+++ src/codejam/year2008/round3/d/solution_test.go
@@ -9,15 +9,29 @@ func runSmall(t *testing.T, H, W, R int, rocks [][]int, expect int) {
 	}
 }
 
+func runSample(t *testing.T, H, W, R int, rocks [][]int, expect int) {
+	rcks := make([][]int64, R)
+	for i := 0; i < R; i++ {
+		rcks[i] = []int64{
+			int64(rocks[i][0]), int64(rocks[i][1]),
+		}
+	}
+	res := solve(int64(H), int64(W), R, rcks)
+	if res != expect {
+		t.Errorf("Sample %d %d %d %v, expect %d, but got %d", H, W, R, rocks, expect, res)
+	}
+}
+
 func TestSample1(t *testing.T) {
 	H, W, R := 1, 1, 0
-	runSmall(t, H, W, R, nil, 1)
+	// runSmall(t, H, W, R, nil, 1)
+	runSample(t, H, W, R, nil, 1)
 }
 
 func TestSample2(t *testing.T) {
 	H, W, R := 4, 4, 1
 	rocks := [][]int{{2, 1}}
-	runSmall(t, H, W, R, rocks, 2)
+	runSample(t, H, W, R, rocks, 2)
 }
 
 func TestSample3(t *testing.T) {

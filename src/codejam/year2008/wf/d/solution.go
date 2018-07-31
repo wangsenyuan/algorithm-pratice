4d5abe251b66fd10a40b7849416fc377ca20dfcd
wangsenyuan <wang.senyuan@gmail.com>
2018-07-31 08:34:24 +0800

    aaa

--- src/codejam/year2008/wf/d/solution.go
+++ src/codejam/year2008/wf/d/solution.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"container/heap"
 	"fmt"
-	"os"
 	"bufio"
+	"os"
+	"math"
 )
 
 func readInt(bytes []byte, from int, val *int) int {
@@ -49,16 +49,21 @@ func readNNums(scanner *bufio.Scanner, n int) []int {
 }
 
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-	tc := readNum(scanner)
+	//scanner := bufio.NewScanner(os.Stdin)
+	//tc := readNum(scanner)
+	var tc int
+	fmt.Scanf("%d", &tc)
 
 	for x := 1; x <= tc; x++ {
-		N, M := readTwoNums(scanner)
-		G := make([][]byte, N)
+		var N, M int
+		fmt.Scanf("%d %d", &N, &M)
+		G := make([]string, N)
 		for i := 0; i < N; i++ {
-			scanner.Scan()
-			G[i] = scanner.Bytes()
+			//scanner.Scan()
+			fmt.Scanf("%s", &G[i])
 		}
+		//fmt.Fprintf(os.Stderr, "[debug] %d %d %v\n", N, M, G)
+
 		res := solve(N, M, G)
 		fmt.Printf("Case #%d: %d\n", x, res)
 	}
@@ -66,156 +71,111 @@ func main() {
 
 var dd = []int{-1, 0, 1, 0, -1}
 
-func solve(N int, M int, G [][]byte) int {
-	cells := make([][]*Cell, N)
+func solve(N int, M int, G []string) int {
+	forest := make([]int, N*M)
+	var idx int
 	for i := 0; i < N; i++ {
-		cells[i] = make([]*Cell, M)
 		for j := 0; j < M; j++ {
-			cells[i][j] = &Cell{cost: -1, x: i, y: j}
+			if G[i][j] == 'T' {
+				forest[idx] = i*M + j
+				idx++
+			}
 		}
-	}
-
-	parent := make([]int, N*M)
-
-	res := make([][]int, N)
-	for i := 0; i < N; i++ {
-		res[i] = make([]int, M)
-		for j := 0; j < M; j++ {
-			res[i][j] = -1
-		}
-	}
-
-	vis := make([][]int, N)
-	for i := 0; i < N; i++ {
-		vis[i] = make([]int, M)
 	}
 
 	que := make([]int, N*M)
-	front, end := 0, 0
-	que[end] = 0
-	end++
-
-	pq := make(Cells, 0, N*M)
-
-	bfs := func(flag int) {
-		for pq.Len() > 0 {
-			u := heap.Pop(&pq).(*Cell)
-			if vis[u.x][u.y] == flag {
-				continue
+	dist := make([]int, N*M)
+	vis := make([]int, N*M)
+	distance := func(x, y int, flag int) int {
+		front, end := 0, 0
+		que[end] = x
+		end++
+		vis[x] = flag
+		dist[x] = 0
+		for front < end {
+			u := que[front]
+			front++
+			if u == y {
+				return dist[u]
 			}
-			vis[u.x][u.y] = flag
-
+			a, b := u/M, u%M
 			for k := 0; k < 4; k++ {
-				a, b := u.x+dd[k], u.y+dd[k+1]
-				if a >= 0 && a < N && b >= 0 && b < M && G[a][b] != '.' {
-					if cells[a][b].cost < 0 || cells[a][b].cost > cells[u.x][u.y].cost+1 {
-						cells[a][b].cost = cells[u.x][u.y].cost + 1
-						heap.Push(&pq, cells[a][b])
-						parent[a*M+b] = u.x*M + u.y
-						if G[a][b] == 'T' {
-							v := a*M + b
-							for {
-								if res[v/M][v%M] < 0 {
-									res[v/M][v%M] = cells[v/M][v%M].cost
-								}
-								v = parent[v]
-								if G[v/M][v%M] == 'T' {
-									break
-								}
-							}
-							que[end] = a*M + b
-							end++
-							return
-						}
-					}
+				c, d := a+dd[k], b+dd[k+1]
+				if c >= 0 && c < N && d >= 0 && d < M && G[c][d] != '.' && vis[c*M+d] != flag {
+					dist[c*M+d] = dist[u] + 1
+					vis[c*M+d] = flag
+					que[end] = c*M + d
+					end++
 				}
 			}
 		}
+		return -1
 	}
-
-	bfs2 := func(flag int) {
-		for pq.Len() > 0 {
-			u := heap.Pop(&pq).(*Cell)
-			if vis[u.x][u.y] == flag {
-				continue
-			}
-			vis[u.x][u.y] = flag
-			for k := 0; k < 4; k++ {
-				a, b := u.x+dd[k], u.y+dd[k+1]
-				if a >= 0 && a < N && b >= 0 && b < M && G[a][b] != '.' {
-					if cells[a][b].cost < 0 || cells[a][b].cost > cells[u.x][u.y].cost+1 {
-						cells[a][b].cost = cells[u.x][u.y].cost + 1
-						heap.Push(&pq, cells[a][b])
-					}
-				}
-			}
-		}
-	}
-
+	ff := make([]int, idx*idx)
 	var flag int
-	for front < end {
-		head := que[front]
-		front++
-		x, y := head/M, head%M
-		cell := cells[x][y]
-		cell.cost = 0
-		heap.Push(&pq, cell)
-		flag++
-		bfs(flag)
+	for i := 0; i < idx; i++ {
+		for j := i + 1; j < idx; j++ {
+			flag++
+			x := distance(forest[i], forest[j], flag)
+			y := x / 2
+			c := x * (x + 1) / 2
+			c -= y * (y + 1)
+			if x&1 == 0 {
+				c += y
+			}
+			ff[i*idx+j] = c
+			ff[j*idx+i] = c
+		}
 	}
 
-	flag++
-	bfs2(flag)
+	mstOfForest := func() int {
+		var res int
+		dd := make([]int, idx)
+		for i := 0; i < idx; i++ {
+			dd[i] = math.MaxInt32
+		}
+		vis := make([]bool, idx)
+		dd[0] = 0
+		for {
+			i := -1
+			for j := 0; j < idx; j++ {
+				if !vis[j] && (i == -1 || dd[j] < dd[i]) {
+					i = j
+				}
+			}
+			if i < 0 {
+				return res
+			}
+			res += dd[i]
+			vis[i] = true
 
-	var ans int
+			for j := 0; j < idx; j++ {
+				if !vis[j] && dd[j] > ff[i*idx+j] {
+					dd[j] = ff[i*idx+j]
+				}
+			}
+		}
+	}
+
+	res := mstOfForest()
+
 	for i := 0; i < N; i++ {
 		for j := 0; j < M; j++ {
-			if res[i][j] < 0 {
-				res[i][j] = cells[i][j].cost
-			}
-			if G[i][j] != '.' {
-				ans += res[i][j]
+			if G[i][j] == '#' {
+				d := -1
+				for k := 0; k < idx; k++ {
+					flag++
+					x := distance(forest[k], i*M+j, flag)
+					if d < 0 || x < d {
+						d = x
+					}
+				}
+				res += d
 			}
 		}
 	}
-	//fmt.Fprintf(os.Stderr, "[debug]cost result %v\n", res)
-	return ans
-}
 
-type Cell struct {
-	cost  int
-	x     int
-	y     int
-	index int
-}
-
-type Cells []*Cell
-
-func (cells Cells) Len() int {
-	return len(cells)
-}
-
-func (cells Cells) Less(i, j int) bool {
-	return cells[i].cost < cells[j].cost
-}
-
-func (cells Cells) Swap(i, j int) {
-	cells[i], cells[j] = cells[j], cells[i]
-	cells[i].index = i
-	cells[j].index = j
-}
-
-func (cells *Cells) Push(item interface{}) {
-	cell := item.(*Cell)
-	cell.index = len(*cells)
-	*cells = append(*cells, cell)
-}
-
-func (cells *Cells) Pop() interface{} {
-	old := *cells
-	n := len(old)
-	last := old[n-1]
-	*cells = old[:n-1]
-	last.index = -1
-	return last
+	fmt.Fprintf(os.Stderr, "[debug]cost result %v\n", res)
+	//fmt.Fprintf(os.Stderr, "[debug] %d %d => %d\n", N, M, ans)
+	return res
 }

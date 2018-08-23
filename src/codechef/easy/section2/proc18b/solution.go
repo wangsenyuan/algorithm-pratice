92fc6241b7727457db52088bc22097ff1636223a
wangsenyuan <wang.senyuan@gmail.com>
2018-08-23 21:46:18 +0800

    aa

---
+++ src/codechef/easy/section2/proc18b/solution.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	tc := readNum(scanner)
+
+	for tc > 0 {
+		tc--
+		n := readNum(scanner)
+		A := readNNums(scanner, n)
+		res := solve(n, A)
+		fmt.Printf("%.7f\n", res)
+	}
+}
+
+func readInt(bytes []byte, from int, val *int) int {
+	i := from
+	tmp := 0
+	for i < len(bytes) && bytes[i] != ' ' {
+		tmp = tmp*10 + int(bytes[i]-'0')
+		i++
+	}
+	*val = tmp
+	return i
+}
+
+func readNum(scanner *bufio.Scanner) (a int) {
+	scanner.Scan()
+	readInt(scanner.Bytes(), 0, &a)
+	return
+}
+
+func readTwoNums(scanner *bufio.Scanner) (a int, b int) {
+	scanner.Scan()
+	x := readInt(scanner.Bytes(), 0, &a)
+	readInt(scanner.Bytes(), x+1, &b)
+	return
+}
+
+func readNNums(scanner *bufio.Scanner, n int) []int {
+	res := make([]int, n)
+	x := -1
+	scanner.Scan()
+	for i := 0; i < n; i++ {
+		x = readInt(scanner.Bytes(), x+1, &res[i])
+	}
+	return res
+}
+
+func solve(n int, A []int) float64 {
+	sort.Ints(A)
+	ans := (float64(A[n-1]) + float64(A[n-2])) / 2
+
+	for i := n - 3; i >= 0; i-- {
+		ans = (ans + float64(A[i])) / 2
+	}
+	return ans
+}

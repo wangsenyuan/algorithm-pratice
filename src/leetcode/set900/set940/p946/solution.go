228bb3e0e55cbaf6f980a4cae09c2181c53e65ed
WangSenyuan <brucewang@maycur.com>
2018-11-25 17:41:16 +0800

    aaa

--- src/leetcode/set900/set940/p946/solution.go
+++ src/leetcode/set900/set940/p946/solution.go
@@ -5,6 +5,25 @@ func validateStackSequences(pushed []int, popped []int) bool {
 	if n == 0 {
 		return true
 	}
+	stack := make([]int, n)
+	var p int
+	var j int
+	for i := 0; i < n; i++ {
+		stack[p] = pushed[i]
+		p++
+		for p > 0 && stack[p-1] == popped[j] {
+			j++
+			p--
+		}
+	}
+	return j == n
+}
+
+func validateStackSequences1(pushed []int, popped []int) bool {
+	n := len(pushed)
+	if n == 0 {
+		return true
+	}
 
 	stack := make([]int, n)
 

6bce9a1265d9bca8d26fe918202f69077ba61602
WangSenyuan <brucewang@maycur.com>
2019-05-19 19:22:26 +0800

    code refactor

--- src/leetcode/set1000/set1040/p1047/solution.go
+++ src/leetcode/set1000/set1040/p1047/solution.go
@@ -4,12 +4,37 @@ func removeDuplicates(S string) string {
 	n := len(S)
 	bs := []byte(S)
 
+	var j int
+
+	for i, k := 1, 0; i <= n; i++ {
+		if i == n || bs[i] != bs[i-1] {
+			if (i-k)%2 == 1 {
+				if j > 0 && bs[j-1] == bs[i-1] {
+					// also need to remove
+					j--
+				} else {
+					// keep it
+					bs[j] = bs[i-1]
+					j++
+				}
+			}
+			k = i
+		}
+	}
+
+	return string(bs[:j])
+}
+
+func removeDuplicates1(S string) string {
+	n := len(S)
+	bs := []byte(S)
+
 	for {
 		var j int
 		var k int
 		for i := 1; i <= n; i++ {
 			if i == n || bs[i] != bs[i-1] {
-				if (i - k) % 2 == 1 {
+				if (i-k)%2 == 1 {
 					// keep
 					bs[j] = bs[i-1]
 					j++

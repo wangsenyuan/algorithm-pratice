d89ec1aaf4fda1e15395ccc973b33fdaba8abe24
WangSenyuan <brucewang@maycur.com>
2019-08-23 20:55:41 +0800

    aaa

---
+++ src/codechef/easy/section13/ks1/solution.go
@@ -0,0 +1,131 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func readInt(bytes []byte, from int, val *int) int {
+	i := from
+	sign := 1
+	if bytes[i] == '-' {
+		sign = -1
+		i++
+	}
+	tmp := 0
+	for i < len(bytes) && bytes[i] != ' ' {
+		tmp = tmp*10 + int(bytes[i]-'0')
+		i++
+	}
+	*val = tmp * sign
+	return i
+}
+
+func readNum(scanner *bufio.Scanner) (a int) {
+	scanner.Scan()
+	readInt(scanner.Bytes(), 0, &a)
+	return
+}
+
+func readTwoNums(scanner *bufio.Scanner) (a int, b int) {
+	res := readNNums(scanner, 2)
+	a, b = res[0], res[1]
+	return
+}
+
+func readNNums(scanner *bufio.Scanner, n int) []int {
+	res := make([]int, n)
+	x := 0
+	scanner.Scan()
+	for i := 0; i < n; i++ {
+		for x < len(scanner.Bytes()) && scanner.Bytes()[x] == ' ' {
+			x++
+		}
+		x = readInt(scanner.Bytes(), x, &res[i])
+	}
+	return res
+}
+
+func fillNNums(scanner *bufio.Scanner, n int, res []int) {
+	x := 0
+	scanner.Scan()
+	for i := 0; i < n; i++ {
+		for x < len(scanner.Bytes()) && scanner.Bytes()[x] == ' ' {
+			x++
+		}
+		x = readInt(scanner.Bytes(), x, &res[i])
+	}
+}
+
+func readUint64(bytes []byte, from int, val *uint64) int {
+	i := from
+
+	var tmp uint64
+	for i < len(bytes) && bytes[i] != ' ' {
+		tmp = tmp*10 + uint64(bytes[i]-'0')
+		i++
+	}
+	*val = tmp
+
+	return i
+}
+
+func readInt64(bytes []byte, from int, val *int64) int {
+	i := from
+	var tmp int64
+	for i < len(bytes) && bytes[i] != ' ' {
+		tmp = tmp*10 + int64(bytes[i]-'0')
+		i++
+	}
+	*val = tmp
+	return i
+}
+
+func readNInt64Nums(scanner *bufio.Scanner, n int) []int64 {
+	res := make([]int64, n)
+	x := -1
+	scanner.Scan()
+	for i := 0; i < n; i++ {
+		x = readInt64(scanner.Bytes(), x+1, &res[i])
+	}
+	return res
+}
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	tc := readNum(scanner)
+
+	for tc > 0 {
+		tc--
+		n := readNum(scanner)
+		A := readNNums(scanner, n)
+		res := solve(n, A)
+		fmt.Println(res)
+	}
+}
+
+const M = 1000001
+
+var count = make([]int, M)
+var bad = make([]int, M)
+
+func solve(n int, A []int) int64 {
+	for i := 0; i < M; i++ {
+		count[i] = 0
+		bad[i] = 0
+	}
+	count[0] = 1
+	bad[0] = 1
+	var res int64
+	var prev int
+	for i := 1; i <= n; i++ {
+		prev ^= A[i-1]
+		res += int64(count[prev])*int64(i) - int64(bad[prev])
+		count[prev]++
+		bad[prev] += i + 1
+	}
+
+	return res
+}
---
+++ src/codechef/easy/section13/ks1/solution_test.go
@@ -0,0 +1,17 @@
+package main
+
+import "testing"
+
+func runSample(t *testing.T, n int, A []int, expect int64) {
+	res := solve(n, A)
+
+	if res != expect {
+		t.Errorf("Sample %d %v, expect %d, but got %d", n, A, expect, res)
+	}
+}
+
+func TestSample1(t *testing.T) {
+	n := 3
+	A := []int{5, 2, 7}
+	runSample(t, n, A, 2)
+}

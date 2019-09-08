938024a4e2847847c0ecf4bbe5fa6c759690ee73
WangSenyuan <brucewang@maycur.com>
2019-09-08 18:08:38 +0800

    aaa

--- src/leetcode/set1000/set1000/set1100/set1180/p1187/solution.go
+++ src/leetcode/set1000/set1000/set1100/set1180/p1187/solution.go
@@ -2,7 +2,71 @@ package p1187
 
 import "sort"
 
-const INF = 1 << 20
+const INF = 1 << 30
+
+func makeArrayIncreasing2(arr1 []int, arr2 []int) int {
+	m := len(arr1)
+	arr3 := make([]int, len(arr2))
+	copy(arr3, arr2)
+	// arr3[len(arr3)-1] = INF
+	sort.Ints(arr3)
+	n := 0
+
+	// remove duplicates from arr2
+	for i := 1; i <= len(arr3); i++ {
+		if i == len(arr3) || (arr3[i] > arr3[i-1]) {
+			arr3[n] = arr3[i-1]
+			n++
+		}
+	}
+	// dp[i][j] = (arr1[i] ends at arr2[j], operations)
+	// fp[j] = min result before j
+	dp := make([][]int, m)
+	for i := 0; i < m; i++ {
+		dp[i] = make([]int, n)
+		for j := 0; j < n; j++ {
+			dp[i][j] = INF
+		}
+	}
+	for j := 0; j < n; j++ {
+		if arr1[0] > arr3[j] {
+			dp[0][j] = 1
+		} else {
+			dp[0][j] = 0
+		}
+	}
+
+	for i := 0; i < m-1; i++ {
+		for j := 0; j < n-1; j++ {
+			if dp[i][j] < INF {
+				if arr1[i+1] > arr3[j] && arr1[i+1] <= arr3[j+1] {
+					dp[i+1][j+1] = dp[i][j]
+				} else {
+					dp[i+1][j+1] = dp[i][j] + 1
+				}
+			}
+		}
+
+		for j := 1; j < n; j++ {
+			dp[i+1][j] = min(dp[i+1][j], dp[i+1][j-1])
+		}
+	}
+
+	res := dp[m-1][n-1]
+
+	for i := m - 2; i >= 0 && arr1[i+1] > arr1[i]; i-- {
+		for j := n - 1; j >= 0; j-- {
+			if arr1[i+1] > arr3[j] {
+				res = min(res, dp[i][j])
+			}
+		}
+	}
+
+	if res >= INF {
+		return -1
+	}
+	return res
+}
 
 func makeArrayIncreasing(arr1 []int, arr2 []int) int {
 	sort.Ints(arr2)

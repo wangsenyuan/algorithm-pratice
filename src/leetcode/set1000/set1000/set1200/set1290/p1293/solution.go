23179a14ee53b9ed576561940384c9e80f561aa0
WangSenyuan <brucewang@maycur.com>
2019-12-15 16:54:20 +0800

    aaa

--- src/leetcode/set1000/set1000/set1200/set1290/p1293/solution.go
+++ src/leetcode/set1000/set1000/set1200/set1290/p1293/solution.go
@@ -7,6 +7,72 @@ var dd = []int{-1, 0, 1, 0, -1}
 func shortestPath(grid [][]int, k int) int {
 	m := len(grid)
 	n := len(grid[0])
+	dist := make([][][]int, m)
+	for i := 0; i < m; i++ {
+		dist[i] = make([][]int, n)
+		for j := 0; j < n; j++ {
+			dist[i][j] = make([]int, k+1)
+			for u := 0; u <= k; u++ {
+				dist[i][j][u] = -1
+			}
+		}
+	}
+
+	dist[0][0][0] = 0
+
+	N := m * n * (k + 1)
+
+	que := make([]int, N)
+	var end int
+	que[end] = 0
+	end++
+	var front int
+
+	for front < end {
+		cur := que[front]
+		front++
+		c := cur % (k + 1)
+		b := (cur / (k + 1)) % n
+		a := (cur / (k + 1)) / n
+
+		for i := 0; i < 4; i++ {
+			u, v := a+dd[i], b+dd[i+1]
+			if u < 0 || u >= m || v < 0 || v >= n {
+				continue
+			}
+			if grid[u][v] == 0 {
+				if dist[u][v][c] < 0 {
+					dist[u][v][c] = dist[a][b][c] + 1
+					que[end] = u*n*(k+1) + v*(k+1) + c
+					end++
+				}
+			} else if c+1 <= k {
+				if dist[u][v][c+1] < 0 {
+					dist[u][v][c+1] = dist[a][b][c] + 1
+					que[end] = u*n*(k+1) + v*(k+1) + c + 1
+					end++
+				}
+			}
+		}
+	}
+
+	res := -1
+
+	for i := 0; i <= k; i++ {
+		if dist[m-1][n-1][i] < 0 {
+			continue
+		}
+		if res < 0 || res > dist[m-1][n-1][i] {
+			res = dist[m-1][n-1][i]
+		}
+	}
+
+	return res
+}
+
+func shortestPath1(grid [][]int, k int) int {
+	m := len(grid)
+	n := len(grid[0])
 
 	vis := make([][]bool, m)
 

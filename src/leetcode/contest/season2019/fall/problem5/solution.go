e08136de91ef4d247d546086af96bab94440db12
WangSenyuan <brucewang@maycur.com>
2020-01-01 21:41:39 +0800

    code refactor

--- src/leetcode/contest/season2019/fall/problem5/solution.go
+++ src/leetcode/contest/season2019/fall/problem5/solution.go
@@ -1,5 +1,114 @@
 package problem5
 
+const MOD = 1000000007
+
+func bonus1(n int, leadership [][]int, operations [][]int) []int {
+	open := make([]int, n)
+	close := make([]int, n)
+	// tour := make([]int, n)
+
+	outs := make([][]int, n)
+
+	for i := 0; i < n; i++ {
+		outs[i] = make([]int, 0, 2)
+	}
+
+	for _, op := range leadership {
+		a, b := op[0]-1, op[1]-1
+		outs[a] = append(outs[a], b)
+	}
+
+	var dfs func(u int, pos *int)
+
+	dfs = func(u int, pos *int) {
+		open[u] = *pos
+
+		*pos++
+
+		for _, v := range outs[u] {
+			dfs(v, pos)
+		}
+
+		close[u] = *pos - 1
+	}
+
+	dfs(0, new(int))
+
+	bit1 := CreateBitTree(n)
+	bit2 := CreateBitTree(n)
+
+	updateRange := func(v int, l int, r int) {
+		bit1.update(l, v)
+		bit1.update(r+1, -v)
+
+		bit2.update(l, v*(l-1))
+		bit2.update(r+1, -v*r)
+	}
+
+	sum := func(x int) int64 {
+		a := bit1.getSum(x) * int64(x)
+		a %= MOD
+		b := bit2.getSum(x) % MOD
+		a -= b
+		if a < 0 {
+			a += MOD
+		}
+		return a
+	}
+
+	rangeSum := func(l, r int) int {
+		res := sum(r) - sum(l-1)
+		if res < 0 {
+			res += MOD
+		}
+		return int(res % MOD)
+	}
+
+	var res []int
+
+	for _, op := range operations {
+		if op[0] == 1 {
+			updateRange(op[2], open[op[1]-1], open[op[1]-1])
+		} else if op[0] == 2 {
+			updateRange(op[2], open[op[1]-1], close[op[1]-1])
+		} else {
+			res = append(res, rangeSum(open[op[1]-1], close[op[1]-1]))
+		}
+	}
+
+	return res
+}
+
+type BitTree struct {
+	arr []int64
+	n   int
+}
+
+func CreateBitTree(n int) BitTree {
+	arr := make([]int64, n+1)
+	return BitTree{arr, n}
+}
+
+func (bit *BitTree) update(p int, v int) {
+	V := int64(v)
+	p++
+	n := bit.n
+	for p <= n {
+		bit.arr[p] += V
+		p += p & -p
+	}
+}
+
+func (bit BitTree) getSum(p int) int64 {
+	var res int64
+	p++
+	for p > 0 {
+		res += bit.arr[p]
+		p -= p & -p
+	}
+	return res
+}
+
 func bonus(n int, leadership [][]int, operations [][]int) []int {
 	outs := make([][]int, n)
 
@@ -57,8 +166,6 @@ func bonus(n int, leadership [][]int, operations [][]int) []int {
 	return res
 }
 
-const MOD = 1000000007
-
 func modAdd(a *int64, b int64) {
 	*a += b
 	if *a >= MOD {
@@ -74,7 +181,7 @@ type MergeTree struct {
 
 func NewMergeTree(n int) MergeTree {
 	arr := make([]int64, 4*n)
-	lazy := make([]int64, 4 * n)
+	lazy := make([]int64, 4*n)
 	return MergeTree{arr, lazy, n}
 }
 
@@ -90,7 +197,7 @@ func (tree *MergeTree) Update(left, right int, value int64) {
 		}
 		if left <= start && end <= right {
 			// total in the range
-			modAdd(&arr[i], int64(end - start + 1) * value % MOD)
+			modAdd(&arr[i], int64(end-start+1)*value%MOD)
 			modAdd(&lazy[i], value)
 			return
 		}
@@ -98,9 +205,9 @@ func (tree *MergeTree) Update(left, right int, value int64) {
 		loop(2*i+1, start, mid)
 		loop(2*i+2, mid+1, end)
 		arr[i] = 0
-		modAdd(&arr[i], arr[2 * i + 1])
-		modAdd(&arr[i], arr[2 * i + 2])
-		modAdd(&arr[i], int64(end - start + 1) * lazy[i] % MOD)
+		modAdd(&arr[i], arr[2*i+1])
+		modAdd(&arr[i], arr[2*i+2])
+		modAdd(&arr[i], int64(end-start+1)*lazy[i]%MOD)
 	}
 	loop(0, 0, size-1)
 }
@@ -117,16 +224,16 @@ func (tree *MergeTree) Get(left, right int) int64 {
 		var res int64
 		if left <= start && end <= right {
 			modAdd(&res, arr[i])
-			modAdd(&res, int64(end - start + 1) * v % MOD)	
-			return res		
+			modAdd(&res, int64(end-start+1)*v%MOD)
+			return res
 		}
 		v += lazy[i]
 		if v >= MOD {
 			v -= MOD
 		}
 		mid := (start + end) / 2
-		a := loop(2 * i + 1, start, mid, v)
-		b := loop(2 * i + 2, mid + 1, end, v)
+		a := loop(2*i+1, start, mid, v)
+		b := loop(2*i+2, mid+1, end, v)
 		modAdd(&res, a)
 		modAdd(&res, b)
 		return res

b121d7bd8328b2e1943d8abfd79b7b77c0c5ac93
WangSenyuan <brucewang@maycur.com>
2020-01-25 10:44:46 +0800

    aaa

---
+++ src/codechef/easy/section15/dynamo/solution.go
@@ -0,0 +1,164 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func readInt(bytes []byte, from int, val *int) int {
+	i := from
+	sign := 1
+	if bytes[i] == '-' {
+		sign = -1
+		i++
+	}
+	tmp := 0
+	for i < len(bytes) && bytes[i] != ' ' {
+		tmp = tmp*10 + int(bytes[i]-'0')
+		i++
+	}
+	*val = tmp * sign
+	return i
+}
+
+func readNum(scanner *bufio.Scanner) (a int) {
+	scanner.Scan()
+	readInt(scanner.Bytes(), 0, &a)
+	return
+}
+
+func readTwoNums(scanner *bufio.Scanner) (a int, b int) {
+	res := readNNums(scanner, 2)
+	a, b = res[0], res[1]
+	return
+}
+
+func readNNums(scanner *bufio.Scanner, n int) []int {
+	res := make([]int, n)
+	x := 0
+	scanner.Scan()
+	for i := 0; i < n; i++ {
+		for x < len(scanner.Bytes()) && scanner.Bytes()[x] == ' ' {
+			x++
+		}
+		x = readInt(scanner.Bytes(), x, &res[i])
+	}
+	return res
+}
+
+func fillNNums(scanner *bufio.Scanner, n int, res []int) {
+	x := 0
+	scanner.Scan()
+	for i := 0; i < n; i++ {
+		for x < len(scanner.Bytes()) && scanner.Bytes()[x] == ' ' {
+			x++
+		}
+		x = readInt(scanner.Bytes(), x, &res[i])
+	}
+}
+
+func readUint64(bytes []byte, from int, val *uint64) int {
+	i := from
+
+	var tmp uint64
+	for i < len(bytes) && bytes[i] != ' ' {
+		tmp = tmp*10 + uint64(bytes[i]-'0')
+		i++
+	}
+	*val = tmp
+
+	return i
+}
+
+func readInt64(bytes []byte, from int, val *int64) int {
+	i := from
+	var tmp int64
+	for i < len(bytes) && bytes[i] != ' ' {
+		tmp = tmp*10 + int64(bytes[i]-'0')
+		i++
+	}
+	*val = tmp
+	return i
+}
+
+func readNInt64Nums(scanner *bufio.Scanner, n int) []int64 {
+	res := make([]int64, n)
+	x := -1
+	scanner.Scan()
+	for i := 0; i < n; i++ {
+		x = readInt64(scanner.Bytes(), x+1, &res[i])
+	}
+	return res
+}
+
+func readUint64Num(scanner *bufio.Scanner) uint64 {
+	// fmt.Fprintln(os.Stderr, "readUint64Num")
+
+	var res uint64
+	scanner.Scan()
+	readUint64(scanner.Bytes(), 0, &res)
+	return res
+}
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	tc := readNum(scanner)
+
+	for tc > 0 {
+		tc--
+
+		n := readNum(scanner)
+
+		A := readUint64Num(scanner)
+		// the D is at most 1
+		x := pow(10, n)
+
+		X := 2 * x
+
+		S := X + A
+
+		fmt.Println(S)
+
+		B := readUint64Num(scanner)
+
+		X -= B
+
+		// i have to make sure D + E = x
+		// C + D + E = X
+		// D + E = X - C = x
+		// C = X - x
+		C := X - x
+
+		if C >= x {
+			C--
+		}
+
+		fmt.Println(C)
+
+		X -= C
+
+		D := readUint64Num(scanner)
+
+		E := X - D
+
+		fmt.Println(E)
+
+		scanner.Scan()
+	}
+}
+
+func pow(a, b int) uint64 {
+	// fmt.Fprintf(os.Stderr, "pow: %d %d\n", a, b)
+	res := uint64(1)
+	A := uint64(a)
+	for b != 0 {
+		if b&1 == 1 {
+			res *= A
+		}
+		A *= A
+		b >>= 1
+	}
+	return res
+}

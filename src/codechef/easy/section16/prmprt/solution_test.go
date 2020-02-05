0928c8fe7be58c6e54be11bd6d597b805591ec06
WangSenyuan <brucewang@maycur.com>
2020-02-05 23:51:02 +0800

    code refactor

---
+++ src/codechef/easy/section16/prmprt/solution_test.go
@@ -0,0 +1,22 @@
+package main
+
+import "testing"
+
+func runSample(t *testing.T, n int, u, v int, expect int) {
+	res := solve(n, u, v)
+	if res != expect {
+		t.Errorf("Sample %d %d %d, expect %d, but got %d", n, u, v, expect, res)
+	}
+}
+
+func TestSample1(t *testing.T) {
+	runSample(t, 20, 2, 3, 2)
+}
+
+func TestSample2(t *testing.T) {
+	runSample(t, 20, 2, 6, 1)
+}
+
+func TestSample3(t *testing.T) {
+	runSample(t, 20, 2, 9, 3)
+}

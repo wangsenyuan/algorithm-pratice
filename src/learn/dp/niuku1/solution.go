4bb0b708ae33b79b3c40af8e8d423dfdb0404634
WangSenyuan <brucewang@maycur.com>
2020-02-08 10:21:59 +0800

    code refactor

---
+++ src/learn/dp/niuku1/solution.go
@@ -0,0 +1,74 @@
+// 本题为考试单行多行输入输出规范示例，无需提交，不计分。
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	scanner.Scan()
+	buf := scanner.Bytes()
+
+	fmt.Println(solve(buf))
+
+}
+
+func solve(buf []byte) string {
+	if len(buf) == 0 {
+		return "."
+	}
+
+	var i, j int
+
+	n := len(buf)
+	begin := true
+	for i < n {
+		if isLetter(buf[i]) {
+			buf[j] = buf[i]
+
+			if begin {
+				if buf[j] >= 'a' && buf[j] <= 'z' {
+					buf[j] = byte(buf[j] - 'a' + 'A')
+				}
+				begin = false
+			}
+
+			j++
+		} else {
+			if !begin {
+				buf[j] = ' '
+				j++
+			}
+			begin = true
+		}
+
+		i++
+	}
+
+	if buf[j-1] == ' ' {
+		buf[j-1] = '.'
+	} else {
+		buf[j] = '.'
+		j++
+	}
+
+	s := string(buf[:j])
+
+	return s
+}
+
+func isLetter(c byte) bool {
+	if c >= 'a' && c <= 'z' {
+		return true
+	}
+
+	if c >= 'A' && c <= 'Z' {
+		return true
+	}
+
+	return false
+}
---
+++ src/learn/dp/niuku1/solution_test.go
@@ -0,0 +1,27 @@
+package main
+
+import "testing"
+
+func runSample(t *testing.T, s string, expect string) {
+	res := solve([]byte(s))
+
+	if res != expect {
+		t.Errorf("Sample %s, expect %s, but got %s", s, expect, res)
+	}
+}
+
+func TestSample1(t *testing.T) {
+	runSample(t, "", ".")
+}
+
+func TestSample2(t *testing.T) {
+	runSample(t, "Who love?, Solo..", "Who Love Solo.")
+}
+
+func TestSample3(t *testing.T) {
+	runSample(t, "----Who,love???Solo", "Who Love Solo.")
+}
+
+func TestSample4(t *testing.T) {
+	runSample(t, "66666666664123+Who-32didn't love? Solo32..", "Who Didn T Love Solo.")
+}

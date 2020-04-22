4abe0b8f3a02f17b024ef07ac5f95dd2eaab6c83
WangSenyuan <brucewang@maycur.com>
2020-04-22 23:41:19 +0800

    aaa

---
+++ src/codechef/easy/section17/strno/solution.go
@@ -0,0 +1,118 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func readInt(bytes []byte, from int, val *int) int {
+	i := from
+	sign := 1
+	if bytes[i] == '-' {
+		sign = -1
+		i++
+	}
+	tmp := 0
+	for i < len(bytes) && bytes[i] >= '0' && bytes[i] <= '9' {
+		tmp = tmp*10 + int(bytes[i]-'0')
+		i++
+	}
+	*val = tmp * sign
+	return i
+}
+
+func readNum(reader *bufio.Reader) (a int) {
+	bs, _ := reader.ReadBytes('\n')
+	readInt(bs, 0, &a)
+	return
+}
+
+func readTwoNums(reader *bufio.Reader) (a int, b int) {
+	res := readNNums(reader, 2)
+	a, b = res[0], res[1]
+	return
+}
+
+func readThreeNums(reader *bufio.Reader) (a int, b int, c int) {
+	res := readNNums(reader, 3)
+	a, b, c = res[0], res[1], res[2]
+	return
+}
+
+func readNNums(reader *bufio.Reader, n int) []int {
+	res := make([]int, n)
+	x := 0
+	bs, _ := reader.ReadBytes('\n')
+	for i := 0; i < n; i++ {
+		for x < len(bs) && (bs[x] < '0' || bs[x] > '9') && bs[x] != '-' {
+			x++
+		}
+		x = readInt(bs, x, &res[i])
+	}
+	return res
+}
+
+func readUint64(bytes []byte, from int, val *uint64) int {
+	i := from
+
+	var tmp uint64
+	for i < len(bytes) && bytes[i] >= '0' && bytes[i] <= '9' {
+		tmp = tmp*10 + uint64(bytes[i]-'0')
+		i++
+	}
+	*val = tmp
+
+	return i
+}
+
+func fillNNums(bs []byte, n int, res []int) {
+	x := 0
+	for i := 0; i < n; i++ {
+		for x < len(bs) && (bs[x] < '0' || bs[x] > '9') {
+			x++
+		}
+		x = readInt(bs, x, &res[i])
+	}
+}
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+
+	tc := readNum(reader)
+
+	for tc > 0 {
+		tc--
+		x, k := readTwoNums(reader)
+
+		r := solve(x, k)
+
+		if r {
+			fmt.Println("1")
+		} else {
+			fmt.Println("0")
+		}
+	}
+}
+
+func solve(x int, k int) bool {
+	var cnt int
+
+	X := int64(x)
+
+	var i int64 = 2
+
+	for i*i <= X {
+		for X%i == 0 {
+			cnt++
+			X /= i
+		}
+		i++
+	}
+
+	if X > 1 {
+		cnt++
+	}
+
+	return cnt >= k
+}

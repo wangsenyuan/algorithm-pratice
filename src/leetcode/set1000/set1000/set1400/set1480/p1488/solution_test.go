e8e496aef7b4be92765df29b6e12f68b60ee0dbb
WangSenyuan <brucewang@maycur.com>
2020-07-21 15:28:45 +0800

    code refactor

--- src/leetcode/set1000/set1000/set1400/set1480/p1488/solution_test.go
+++ src/leetcode/set1000/set1000/set1400/set1480/p1488/solution_test.go
@@ -29,9 +29,3 @@ func TestSample3(t *testing.T) {
 	rains := []int{1, 2, 0, 1, 2}
 	runSample(t, rains, nil)
 }
-
-func TestSample4(t *testing.T) {
-	rains := []int{69, 0, 0, 0, 69}
-	expect := []int{-1, 69, -1, -1, -1}
-	runSample(t, rains, expect)
-}

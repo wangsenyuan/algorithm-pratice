36a70cef64a458621cc2565e5b238505308652e5
WangSenyuan <brucewang@maycur.com>
2020-08-28 22:19:05 +0800

    aaa

--- src/codechef/medium/section12/sereja/solution.go
+++ src/codechef/medium/section12/sereja/solution.go
@@ -85,6 +85,117 @@ const N_INF = -INF
 
 var prod [MAX_N]int64
 
+const H = 15
+
+func solve(n int, A []int) int {
+	rmq := make([][]int, n)
+	rxq := make([][]int, n)
+	for i := 0; i < n; i++ {
+		rmq[i] = make([]int, H)
+		rmq[i][0] = i
+		rxq[i] = make([]int, H)
+		rxq[i][0] = i
+	}
+	for j := 1; j < H; j++ {
+		for i := 0; i < n; i++ {
+			rmq[i][j] = rmq[i][j-1]
+			k := i + (1 << uint(j-1))
+			if k < n && A[rmq[i][j-1]] > A[rmq[k][j-1]] {
+				rmq[i][j] = rmq[k][j-1]
+			}
+			rxq[i][j] = rxq[i][j-1]
+
+			if k < n && A[rxq[i][j-1]] < A[rxq[k][j-1]] {
+				rxq[i][j] = rxq[k][j-1]
+			}
+		}
+	}
+
+	K := make([]int, n+1)
+	var cur int
+	for i := 1; i <= n; i++ {
+		if 1<<uint(cur+1) == i {
+			cur++
+		}
+		K[i] = cur
+	}
+
+	findMaxPos := func(i, j int) int {
+		l := j - i + 1
+		k := K[l]
+		u := rxq[i][k]
+		v := rxq[j-(1<<uint(k))+1][k]
+		if A[u] > A[v] {
+			return u
+		}
+		return v
+	}
+
+	findMinPos := func(i, j int) int {
+		l := j - i + 1
+		k := K[l]
+		u := rmq[i][k]
+		v := rmq[j-(1<<uint(k))+1][k]
+		if A[u] < A[v] {
+			return u
+		}
+		return v
+	}
+
+	mem := make(map[int64]int64)
+
+	var dp func(i, j int) int64
+
+	dp = func(i, j int) int64 {
+		ij := int64(i)*int64(n) + int64(j)
+		if r, found := mem[ij]; found {
+			return r
+		}
+		if j <= i {
+			return 1
+		}
+		a := findMaxPos(i, j)
+		b := findMinPos(i, j)
+		d := int64(A[a] - A[b])
+		a, b = min(a, b), max(a, b)
+		res := pow(d, int64(a-i+1)*int64(j-b+1))
+		res = (res * dp(i, b-1)) % MOD
+		res = (res * dp(a+1, j)) % MOD
+		res = (res * inverse(dp(a+1, b-1))) % MOD
+		mem[ij] = res
+		return res
+	}
+
+	res := dp(0, n-1)
+
+	return int(res)
+}
+
+func pow(a, b int64) int64 {
+	var r int64 = 1
+	for b > 0 {
+		if b&1 == 1 {
+			r *= a
+			r %= MOD
+		}
+		a *= a
+		a %= MOD
+		b >>= 1
+	}
+	return r
+}
+
+func inverse(num int64) int64 {
+	return pow(num, MOD-2)
+}
+
+func max(a, b int) int {
+	if a >= b {
+		return a
+	}
+	return b
+}
+
 func solve2(n int, A []int) int {
 
 	prod[0] = 1
@@ -117,7 +228,7 @@ func solve2(n int, A []int) int {
 	return int(res)
 }
 
-func solve(n int, A []int) int {
+func solve1(n int, A []int) int {
 
 	prod[0] = 1
 	var res int64 = 1

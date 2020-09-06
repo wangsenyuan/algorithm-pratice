1d47c9180adf25d5cda587567d1c7ab502170841
WangSenyuan <brucewang@maycur.com>
2020-09-06 14:12:14 +0800

    code refactor

--- src/leetcode/set1000/set1000/set1570/p1574/solution.go
+++ src/leetcode/set1000/set1000/set1570/p1574/solution.go
@@ -1,29 +1,23 @@
 package p1574
 
-import "sort"
-
 func findLengthOfShortestSubarray(arr []int) int {
 	n := len(arr)
 	j := n - 1
 	for j > 0 && arr[j-1] <= arr[j] {
 		j--
 	}
-	if j == 0 {
-		return 0
-	}
+	cur := j
 	// j is the starting point of non-decreasing suffix
 	var best = j
-	for i := 0; i < j; i++ {
+	for i := 0; i < cur; i++ {
 		// if we delete after i, the answer is
-		x := arr[i]
-		k := sort.Search(n-j, func(k int) bool {
-			return arr[j+k] >= x
-		})
-		k += j
-		if k-i-1 < best {
-			best = k - i - 1
+		for j < n && arr[j] < arr[i] {
+			j++
 		}
-		if arr[i+1] < arr[i] {
+		if j-i-1 < best {
+			best = j - i - 1
+		}
+		if arr[i] > arr[i+1] {
 			break
 		}
 	}

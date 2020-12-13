1f66871c76288ffd9e11934893628d0145df9014
WangSenyuan <brucewang@maycur.com>
2020-12-13 20:29:25 +0800

    code refactor

--- src/leetcode/contest/season2020/week219/p5612/solution.go
+++ src/leetcode/contest/season2020/week219/p5612/solution.go
@@ -4,6 +4,81 @@ const INF = 1 << 30
 
 func boxDelivering(boxes [][]int, portsCount int, maxBoxes int, maxWeight int) int {
 	n := len(boxes)
+	N := n + 1
+	arr := make([]int, 2*N)
+
+	for i := 0; i < len(arr); i++ {
+		arr[i] = INF
+	}
+
+	update := func(p int, v int) {
+		p += N
+		arr[p] = v
+		for p > 0 {
+			arr[p>>1] = min(arr[p], arr[p^1])
+			p >>= 1
+		}
+	}
+
+	get := func(l int, r int) int {
+		l += N
+		r += N
+		var res = INF
+		for l < r {
+			if l&1 == 1 {
+				res = min(res, arr[l])
+				l++
+			}
+			if r&1 == 1 {
+				r--
+				res = min(res, arr[r])
+			}
+			l >>= 1
+			r >>= 1
+		}
+		return res
+	}
+
+	diff := make([]int, n+1)
+
+	for i := 1; i <= n; i++ {
+		diff[i] = diff[i-1]
+		if i > 1 && boxes[i-1][0] != boxes[i-2][0] {
+			diff[i]++
+		}
+	}
+	var ans int
+	var w int
+	update(0, 0)
+	for i, j := 1, 0; i <= n; i++ {
+		w += boxes[i-1][1]
+
+		for i-j > maxBoxes || w > maxWeight {
+			w -= boxes[j][1]
+			j++
+		}
+
+		tmp := get(j, i)
+
+		ans = tmp + diff[i] + 2
+
+		if i < n {
+			update(i, ans-diff[i+1])
+		}
+	}
+
+	return ans
+}
+
+func min(a, b int) int {
+	if a <= b {
+		return a
+	}
+	return b
+}
+
+func boxDelivering1(boxes [][]int, portsCount int, maxBoxes int, maxWeight int) int {
+	n := len(boxes)
 
 	diff := make([]int, n+1)
 
@@ -20,8 +95,7 @@ func boxDelivering(boxes [][]int, portsCount int, maxBoxes int, maxWeight int) i
 	var front, end int
 	que[end] = 0
 	end++
-
-	f := make([]int, n+1)
+	var res int
 	g := make([]int, n+1)
 
 	for i := 1; i <= n; i++ {
@@ -31,10 +105,10 @@ func boxDelivering(boxes [][]int, portsCount int, maxBoxes int, maxWeight int) i
 			front++
 		}
 
-		f[i] = g[que[front]] + diff[i] + 2
+		res = g[que[front]] + diff[i] + 2
 
 		if i < n {
-			g[i] = f[i] - diff[i+1]
+			g[i] = res - diff[i+1]
 			for front < end && g[i] <= g[que[end-1]] {
 				end--
 			}
@@ -42,16 +116,5 @@ func boxDelivering(boxes [][]int, portsCount int, maxBoxes int, maxWeight int) i
 			end++
 		}
 	}
-	return f[n]
-}
-
-func min(a, b Pair) Pair {
-	if a.first <= b.first {
-		return a
-	}
-	return b
-}
-
-type Pair struct {
-	first, second int
+	return res
 }

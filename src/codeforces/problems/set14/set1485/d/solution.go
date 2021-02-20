7782010e7252342d6864cf2d2e2072d29d448d09
WangSenyuan <brucewang@maycur.com>
2021-02-20 21:35:00 +0800

    aaa

---
+++ src/codeforces/problems/set14/set1485/d/solution.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"os"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+
+	var buf bytes.Buffer
+
+	n, m := readTwoNums(reader)
+
+	A := make([][]int, n)
+	for i := 0; i < n; i++ {
+		A[i] = readNNums(reader, m)
+	}
+	solve(A)
+
+	for i := 0; i < n; i++ {
+		for j := 0; j < m; j++ {
+			buf.WriteString(fmt.Sprintf("%d ", A[i][j]))
+		}
+		buf.WriteByte('\n')
+	}
+	fmt.Print(buf.String())
+}
+
+func readInt(bytes []byte, from int, val *int) int {
+	i := from
+	sign := 1
+	if bytes[i] == '-' {
+		sign = -1
+		i++
+	}
+	tmp := 0
+	for i < len(bytes) && bytes[i] >= '0' && bytes[i] <= '9' {
+		tmp = tmp*10 + int(bytes[i]-'0')
+		i++
+	}
+	*val = tmp * sign
+	return i
+}
+
+func readNum(reader *bufio.Reader) (a int) {
+	bs, _ := reader.ReadBytes('\n')
+	readInt(bs, 0, &a)
+	return
+}
+
+func readTwoNums(reader *bufio.Reader) (a int, b int) {
+	res := readNNums(reader, 2)
+	a, b = res[0], res[1]
+	return
+}
+
+func readThreeNums(reader *bufio.Reader) (a int, b int, c int) {
+	res := readNNums(reader, 3)
+	a, b, c = res[0], res[1], res[2]
+	return
+}
+
+func readNNums(reader *bufio.Reader, n int) []int {
+	res := make([]int, n)
+	x := 0
+	bs, _ := reader.ReadBytes('\n')
+	for i := 0; i < n; i++ {
+		for x < len(bs) && (bs[x] < '0' || bs[x] > '9') && bs[x] != '-' {
+			x++
+		}
+		x = readInt(bs, x, &res[i])
+	}
+	return res
+}
+
+func readUint64(bytes []byte, from int, val *uint64) int {
+	i := from
+
+	var tmp uint64
+	for i < len(bytes) && bytes[i] >= '0' && bytes[i] <= '9' {
+		tmp = tmp*10 + uint64(bytes[i]-'0')
+		i++
+	}
+	*val = tmp
+
+	return i
+}
+
+const H = 720720 // lcm(1, 2, 3,... 16)
+
+func solve(A [][]int) {
+	for i := 0; i < len(A); i++ {
+		for j := 0; j < len(A[i]); j++ {
+			if (i+j)&1 == 0 {
+				A[i][j] = H
+			} else {
+				A[i][j] = H + pow4(A[i][j])
+			}
+		}
+	}
+}
+
+func pow4(a int) int {
+	return a * a * a * a
+}

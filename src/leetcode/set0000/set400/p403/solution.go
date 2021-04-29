50c27a70d362e3d9a490a395802ddb09a551deeb
WangSenyuan <brucewang@maycur.com>
2021-04-29 23:17:30 +0800

    aaa

--- src/leetcode/set0000/set400/p403/solution.go
+++ src/leetcode/set0000/set400/p403/solution.go
@@ -1,14 +1,55 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func main() {
-	//fmt.Println(canCross([]int{0, 1, 3, 5, 6, 8, 12, 17}))
-	//fmt.Println(canCross([]int{0, 1, 2, 3, 4, 8, 9, 11}))
+	fmt.Println(canCross([]int{0, 1, 3, 5, 6, 8, 12, 17}))
+	fmt.Println(canCross([]int{0, 1, 2, 3, 4, 8, 9, 11}))
 	fmt.Println(canCross([]int{0, 1, 3, 6, 7}))
 }
 
 func canCross(stones []int) bool {
+	n := len(stones)
+	dp := make([][]bool, n+1)
+	// k will at most n
+	for i := 0; i < n; i++ {
+		dp[i] = make([]bool, n)
+	}
+
+	dp[0][0] = true
+
+	find := func(i, k int) {
+		j := sort.Search(n, func(j int) bool {
+			return stones[j] >= stones[i]+k
+		})
+		if j < n && stones[j]-stones[i] == k {
+			dp[j][k] = true
+		}
+	}
+
+	for i := 0; i+1 < n; i++ {
+		for k := 0; k <= i; k++ {
+			if dp[i][k] {
+				find(i, k+1)
+				find(i, k)
+				if k > 0 {
+					find(i, k-1)
+				}
+			}
+		}
+	}
+	for k := 0; k < n; k++ {
+		if dp[n-1][k] {
+			return true
+		}
+	}
+	return false
+}
+
+func canCross1(stones []int) bool {
 	n := len(stones)
 	if n <= 1 {
 		return true

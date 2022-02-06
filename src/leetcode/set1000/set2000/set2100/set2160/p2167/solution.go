369e74cc8683d99e9c1159b5d3e9dc82b7247072
WangSenyuan <brucewang@maycur.com>
2022-02-06 18:55:24 +0800

    aaa

--- src/leetcode/set1000/set2000/set2100/set2160/p2167/solution.go
+++ src/leetcode/set1000/set2000/set2100/set2160/p2167/solution.go
@@ -11,49 +11,16 @@ func minimumTime(s string) int {
 	// i + 1 + 2 * (sum(j) - sum[i]) + n - j - 1
 	// i + 2 * sum(j) - 2 * sum[i]
 	// i - 2 * sumn[i] 最小
-	N := n + 1
-	arr := make([]int, 2*N)
-
-	for i := 0; i < len(arr); i++ {
-		arr[i] = INF
-	}
-
-	set := func(p int, v int) {
-		p += N
-		arr[p] = v
-		for p > 0 {
-			arr[p>>1] = min(arr[p], arr[p^1])
-			p >>= 1
-		}
-	}
-
-	get := func(l, r int) int {
-		res := INF
-		l += N
-		r += N
-		for l < r {
-			if l&1 == 1 {
-				res = min(res, arr[l])
-				l++
-			}
-			if r&1 == 1 {
-				r--
-				res = min(res, arr[r])
-			}
-			l >>= 1
-			r >>= 1
-		}
-		return res
-	}
-	set(0, 0)
+	var minValue int
 	best := n
 	var sum int
 	for i := 1; i <= n; i++ {
 		sum += int(s[i-1] - '0')
 
-		best = min(best, n-i+2*sum+get(0, i))
-
-		set(i, i-2*sum)
+		best = min(best, n-i+2*sum+minValue)
+		if i-2*sum < minValue {
+			minValue = i - 2*sum
+		}
 	}
 	return best
 }

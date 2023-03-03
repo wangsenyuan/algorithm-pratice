f22bffb97d0a38fb8390b7f14e6c35361069e95e
WangSenyuan <brucewang@maycur.com>
2023-03-03 23:26:56 +0800

    aaa

---
+++ src/codeforces/set1/set16/set161/set1613/c/solution.go
@@ -0,0 +1,159 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"os"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	tc := readNum(reader)
+
+	var buf bytes.Buffer
+
+	for tc > 0 {
+		tc--
+		var n int
+		var h int64
+		s, _ := reader.ReadBytes('\n')
+		pos := readInt(s, 0, &n)
+		readInt64(s, pos+1, &h)
+		A := readNNums(reader, n)
+		res := solve(A, h)
+
+		buf.WriteString(fmt.Sprintf("%d\n", res))
+	}
+
+	fmt.Print(buf.String())
+}
+
+func readString(reader *bufio.Reader) string {
+	s, _ := reader.ReadString('\n')
+	for i := 0; i < len(s); i++ {
+		if s[i] == '\n' {
+			return s[:i]
+		}
+	}
+	return s
+}
+
+func readNInt64s(reader *bufio.Reader, n int) []int64 {
+	res := make([]int64, n)
+	s, _ := reader.ReadBytes('\n')
+
+	var pos int
+
+	for i := 0; i < n; i++ {
+		pos = readInt64(s, pos, &res[i]) + 1
+	}
+
+	return res
+}
+
+func readInt64(bytes []byte, from int, val *int64) int {
+	i := from
+	var sign int64 = 1
+	if bytes[i] == '-' {
+		sign = -1
+		i++
+	}
+	var tmp int64
+	for i < len(bytes) && bytes[i] >= '0' && bytes[i] <= '9' {
+		tmp = tmp*10 + int64(bytes[i]-'0')
+		i++
+	}
+	*val = tmp * sign
+	return i
+}
+
+func readInt(bytes []byte, from int, val *int) int {
+	i := from
+	sign := 1
+	if bytes[i] == '-' {
+		sign = -1
+		i++
+	}
+	tmp := 0
+	for i < len(bytes) && bytes[i] >= '0' && bytes[i] <= '9' {
+		tmp = tmp*10 + int(bytes[i]-'0')
+		i++
+	}
+	*val = tmp * sign
+	return i
+}
+
+func readNum(reader *bufio.Reader) (a int) {
+	bs, _ := reader.ReadBytes('\n')
+	readInt(bs, 0, &a)
+	return
+}
+
+func readTwoNums(reader *bufio.Reader) (a int, b int) {
+	res := readNNums(reader, 2)
+	a, b = res[0], res[1]
+	return
+}
+
+func readThreeNums(reader *bufio.Reader) (a int, b int, c int) {
+	res := readNNums(reader, 3)
+	a, b, c = res[0], res[1], res[2]
+	return
+}
+
+func readNNums(reader *bufio.Reader, n int) []int {
+	res := make([]int, n)
+	x := 0
+	bs, _ := reader.ReadBytes('\n')
+	for i := 0; i < n; i++ {
+		for x < len(bs) && (bs[x] < '0' || bs[x] > '9') && bs[x] != '-' {
+			x++
+		}
+		x = readInt(bs, x, &res[i])
+	}
+	return res
+}
+
+func readUint64(bytes []byte, from int, val *uint64) int {
+	i := from
+
+	var tmp uint64
+	for i < len(bytes) && bytes[i] >= '0' && bytes[i] <= '9' {
+		tmp = tmp*10 + uint64(bytes[i]-'0')
+		i++
+	}
+	*val = tmp
+
+	return i
+}
+
+func solve(A []int, h int64) int64 {
+	n := len(A)
+	check := func(k int64) bool {
+		var demage int64
+
+		for i := 0; i < n; i++ {
+			b := k
+			if i+1 < n && int64(A[i+1]-A[i]) < k {
+				b = int64(A[i+1] - A[i])
+			}
+			demage += b
+		}
+
+		return demage >= h
+	}
+
+	var l int64
+	r := h
+
+	for l < r {
+		mid := (l + r) / 2
+		if check(mid) {
+			r = mid
+		} else {
+			l = mid + 1
+		}
+	}
+	return r
+}

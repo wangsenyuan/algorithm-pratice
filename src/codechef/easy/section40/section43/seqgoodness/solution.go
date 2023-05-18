98fe327b8aac95b32f3902edf19311ef603ddfee
WangSenyuan <brucewang@maycur.com>
2023-05-18 10:40:08 +0800

    aa

---
+++ src/codechef/easy/section40/section43/seqgoodness/solution.go
@@ -0,0 +1,185 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"os"
+	"sort"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+
+	tc := readNum(reader)
+	var buf bytes.Buffer
+
+	for tc > 0 {
+		tc--
+		n := readNum(reader)
+		A := readNNums(reader, n)
+		res := solve(A)
+		buf.WriteString(fmt.Sprintf("%d\n", res))
+	}
+
+	fmt.Print(buf.String())
+}
+
+func readString(reader *bufio.Reader) string {
+	s, _ := reader.ReadString('\n')
+	for i := 0; i < len(s); i++ {
+		if s[i] == '\n' {
+			return s[:i]
+		}
+	}
+	return s
+}
+
+func readNInt64s(reader *bufio.Reader, n int) []int64 {
+	res := make([]int64, n)
+	s, _ := reader.ReadBytes('\n')
+
+	var pos int
+
+	for i := 0; i < n; i++ {
+		pos = readInt64(s, pos, &res[i]) + 1
+	}
+
+	return res
+}
+
+func readInt64(bytes []byte, from int, val *int64) int {
+	i := from
+	var sign int64 = 1
+	if bytes[i] == '-' {
+		sign = -1
+		i++
+	}
+	var tmp int64
+	for i < len(bytes) && bytes[i] >= '0' && bytes[i] <= '9' {
+		tmp = tmp*10 + int64(bytes[i]-'0')
+		i++
+	}
+	*val = tmp * sign
+	return i
+}
+
+func readInt(bytes []byte, from int, val *int) int {
+	i := from
+	sign := 1
+	if bytes[i] == '-' {
+		sign = -1
+		i++
+	}
+	tmp := 0
+	for i < len(bytes) && bytes[i] >= '0' && bytes[i] <= '9' {
+		tmp = tmp*10 + int(bytes[i]-'0')
+		i++
+	}
+	*val = tmp * sign
+	return i
+}
+
+func readNum(reader *bufio.Reader) (a int) {
+	bs, _ := reader.ReadBytes('\n')
+	readInt(bs, 0, &a)
+	return
+}
+
+func readTwoNums(reader *bufio.Reader) (a int, b int) {
+	res := readNNums(reader, 2)
+	a, b = res[0], res[1]
+	return
+}
+
+func readThreeNums(reader *bufio.Reader) (a int, b int, c int) {
+	res := readNNums(reader, 3)
+	a, b, c = res[0], res[1], res[2]
+	return
+}
+
+func readNNums(reader *bufio.Reader, n int) []int {
+	res := make([]int, n)
+	x := 0
+	bs, _ := reader.ReadBytes('\n')
+	for i := 0; i < n; i++ {
+		for x < len(bs) && (bs[x] < '0' || bs[x] > '9') && bs[x] != '-' {
+			x++
+		}
+		x = readInt(bs, x, &res[i])
+	}
+	return res
+}
+
+const MOD = 1e9 + 7
+const N = 1e5 + 10
+
+func add(a, b int) int {
+	a += b
+	if a >= MOD {
+		a -= MOD
+	}
+	return a
+}
+
+func mul(a, b int) int {
+	return int(int64(a) * int64(b) % MOD)
+}
+
+func pow(a, b int) int {
+	r := 1
+	for b > 0 {
+		if b&1 == 1 {
+			r = mul(r, a)
+		}
+		a = mul(a, a)
+		b >>= 1
+	}
+	return r
+}
+
+func inverse(a int) int {
+	return pow(a, MOD-2)
+}
+
+var F []int
+var I []int
+
+func init() {
+	F = make([]int, N)
+	F[0] = 1
+	for i := 1; i < N; i++ {
+		F[i] = mul(i, F[i-1])
+	}
+
+	I = make([]int, N)
+	I[N-1] = inverse(F[N-1])
+	for i := int(N - 2); i >= 0; i-- {
+		I[i] = mul(i+1, I[i+1])
+	}
+}
+
+func nCr(n int, r int) int {
+	if r < 0 || n < r {
+		return 0
+	}
+	return mul(F[n], mul(I[r], I[n-r]))
+}
+
+func solve(A []int) int {
+	// 考虑A[j]的贡献， 如果 A[j] 存入的时候已经有A[j]-1个数
+	// 那么它的贡献就是1， 同时后面可以随便放入
+	// nCr(j-1, A[j]-1) * 2 ** (n - j - 1)
+	// 但这里有个问题，可能后面放入的数也可能使它的位置变化。所以要排序
+	sort.Ints(A)
+	var res int
+
+	n := len(A)
+	for i := 0; i < n; i++ {
+		x := nCr(i, A[i]-1)
+		y := pow(2, n-i-1)
+		res = add(res, mul(x, y))
+	}
+
+	return res
+}

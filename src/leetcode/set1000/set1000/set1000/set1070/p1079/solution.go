4ca89828164151f655c096a83593022ba304ffb0
WangSenyuan <brucewang@maycur.com>
2023-05-19 22:46:55 +0800

    aaa

--- src/leetcode/set1000/set1000/set1000/set1070/p1079/solution.go
+++ src/leetcode/set1000/set1000/set1000/set1070/p1079/solution.go
@@ -26,6 +26,55 @@ func init() {
 }
 
 func numTilePossibilities(tiles string) int {
+	cnt := make(map[int]int)
+	for i := 0; i < len(tiles); i++ {
+		cnt[int(tiles[i]-'A')]++
+	}
+
+	id := make([]int, 26)
+	var p int
+	for k := range cnt {
+		id[k] = p
+		p++
+	}
+
+	cnt2 := make([]int, p)
+	for k, v := range cnt {
+		cnt2[id[k]] = v
+	}
+
+	calc := func(use []int) int {
+		var sum int
+		for _, x := range use {
+			sum += x
+		}
+		res := 1
+		for i := 0; i < p; i++ {
+			res *= C[sum][use[i]]
+			sum -= use[i]
+		}
+		return res
+	}
+
+	//n := len(tiles)
+
+	var dfs func(i int, use []int) int
+
+	dfs = func(i int, use []int) int {
+		if i == p {
+			return calc(use)
+		}
+		var ans int
+		for x := 0; x <= cnt2[i]; x++ {
+			use[i] = x
+			ans += dfs(i+1, use)
+		}
+		return ans
+	}
+	return dfs(0, make([]int, p)) - 1
+}
+
+func numTilePossibilities2(tiles string) int {
 	n := len(tiles)
 
 	bs := make([]int, n)

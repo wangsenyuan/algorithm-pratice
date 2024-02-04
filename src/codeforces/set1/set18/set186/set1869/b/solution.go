a88df2ee4d7c5f20725f490255574e87231f897c
WangSenyuan <brucewang@maycur.com>
2024-02-04 22:50:51 +0800

    aaa

---
+++ src/codeforces/set1/set18/set186/set1869/b/solution.go
@@ -0,0 +1,186 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"os"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+
+	tc := readNum(reader)
+
+	var buf bytes.Buffer
+
+	for tc > 0 {
+		tc--
+		nums := readNNums(reader, 4)
+		n, k, a, b := nums[0], nums[1], nums[2], nums[3]
+		points := make([][]int, n)
+		for i := 0; i < n; i++ {
+			points[i] = readNNums(reader, 2)
+		}
+		res := solve(points, k, a, b)
+		buf.WriteString(fmt.Sprintf("%d\n", res))
+	}
+
+	fmt.Print(buf.String())
+}
+
+func readString(reader *bufio.Reader) string {
+	s, _ := reader.ReadString('\n')
+	for i := 0; i < len(s); i++ {
+		if s[i] == '\n' || s[i] == '\r' {
+			return s[:i]
+		}
+	}
+	return s
+}
+
+func readNInt64s(reader *bufio.Reader, n int) []int64 {
+	res := make([]int64, n)
+	s, _ := reader.ReadBytes('\n')
+
+	var pos int
+
+	for i := 0; i < n; i++ {
+		pos = readInt64(s, pos, &res[i]) + 1
+	}
+
+	return res
+}
+
+func readInt64(bytes []byte, from int, val *int64) int {
+	i := from
+	var sign int64 = 1
+	if bytes[i] == '-' {
+		sign = -1
+		i++
+	}
+	var tmp int64
+	for i < len(bytes) && bytes[i] >= '0' && bytes[i] <= '9' {
+		tmp = tmp*10 + int64(bytes[i]-'0')
+		i++
+	}
+	*val = tmp * sign
+	return i
+}
+
+func readInt(bytes []byte, from int, val *int) int {
+	i := from
+	sign := 1
+	if bytes[i] == '-' {
+		sign = -1
+		i++
+	}
+	tmp := 0
+	for i < len(bytes) && bytes[i] >= '0' && bytes[i] <= '9' {
+		tmp = tmp*10 + int(bytes[i]-'0')
+		i++
+	}
+	*val = tmp * sign
+	return i
+}
+
+func readNum(reader *bufio.Reader) (a int) {
+	bs, _ := reader.ReadBytes('\n')
+	readInt(bs, 0, &a)
+	return
+}
+
+func readTwoNums(reader *bufio.Reader) (a int, b int) {
+	res := readNNums(reader, 2)
+	a, b = res[0], res[1]
+	return
+}
+
+func readThreeNums(reader *bufio.Reader) (a int, b int, c int) {
+	res := readNNums(reader, 3)
+	a, b, c = res[0], res[1], res[2]
+	return
+}
+
+func readNNums(reader *bufio.Reader, n int) []int {
+	res := make([]int, n)
+	x := 0
+	bs, _ := reader.ReadBytes('\n')
+	for i := 0; i < n; i++ {
+		for x < len(bs) && (bs[x] < '0' || bs[x] > '9') && bs[x] != '-' {
+			x++
+		}
+		x = readInt(bs, x, &res[i])
+	}
+	return res
+}
+
+func solve(points [][]int, k int, a int, b int) int {
+	// first k are primary
+	a, b = min(a, b), max(a, b)
+	if b <= k {
+		return 0
+	}
+	//n := len(points)
+	// 要找到离a最近的primary city
+	// 再找到离b最近的primary city
+
+	getMinDistFromPrimaryCities := func(i int) int {
+		if i < k {
+			return 0
+		}
+
+		dist := 1 << 60
+
+		for j := 0; j < k; j++ {
+			dist = min(dist, abs(points[i][0]-points[j][0])+abs(points[i][1]-points[j][1]))
+		}
+
+		return dist
+	}
+
+	res := abs(points[b-1][0]-points[a-1][0]) + abs(points[b-1][1]-points[a-1][1])
+
+	res = min(res, getMinDistFromPrimaryCities(a-1)+getMinDistFromPrimaryCities(b-1))
+
+	return res
+}
+
+func abs(num int) int {
+	if num < 0 {
+		return -num
+	}
+	return num
+}
+
+func max(a, b int) int {
+	if a >= b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	return a + b - max(a, b)
+}
+
+type Graph struct {
+	nodes []int
+	next  []int
+	to    []int
+	cur   int
+}
+
+func NewGraph(n int, e int) *Graph {
+	nodes := make([]int, n)
+	next := make([]int, e)
+	to := make([]int, e)
+	return &Graph{nodes, next, to, 0}
+}
+
+func (g *Graph) AddEdge(u, v int) {
+	g.cur++
+	g.next[g.cur] = g.nodes[u]
+	g.nodes[u] = g.cur
+	g.to[g.cur] = v
+}

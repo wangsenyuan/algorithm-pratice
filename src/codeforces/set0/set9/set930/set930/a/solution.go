b5539cf890e739418dd37b197ffd938d8ac3803e
WangSenyuan <brucewang@maycur.com>
2024-03-30 22:52:51 +0800

    aaa

---
+++ src/codeforces/set0/set9/set930/set930/a/solution.go
@@ -0,0 +1,126 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+
+	n := readNum(reader)
+	p := readNNums(reader, n-1)
+
+	res := solve(n, p)
+
+	fmt.Println(res)
+}
+
+func readString(reader *bufio.Reader) string {
+	s, _ := reader.ReadString('\n')
+	for i := 0; i < len(s); i++ {
+		if s[i] == '\r' || s[i] == '\n' {
+			return s[:i]
+		}
+	}
+	return s
+}
+
+func readInt(bytes []byte, from int, val *int) int {
+	i := from
+	sign := 1
+	if bytes[i] == '-' {
+		sign = -1
+		i++
+	}
+	tmp := 0
+	for i < len(bytes) && bytes[i] >= '0' && bytes[i] <= '9' {
+		tmp = tmp*10 + int(bytes[i]-'0')
+		i++
+	}
+	*val = tmp * sign
+	return i
+}
+
+func readNum(reader *bufio.Reader) (a int) {
+	bs, _ := reader.ReadBytes('\n')
+	readInt(bs, 0, &a)
+	return
+}
+
+func readTwoNums(reader *bufio.Reader) (a int, b int) {
+	res := readNNums(reader, 2)
+	a, b = res[0], res[1]
+	return
+}
+
+func readThreeNums(reader *bufio.Reader) (a int, b int, c int) {
+	res := readNNums(reader, 3)
+	a, b, c = res[0], res[1], res[2]
+	return
+}
+
+func readNNums(reader *bufio.Reader, n int) []int {
+	res := make([]int, n)
+	x := 0
+	bs, _ := reader.ReadBytes('\n')
+	for i := 0; i < n; i++ {
+		for x < len(bs) && (bs[x] < '0' || bs[x] > '9') && bs[x] != '-' {
+			x++
+		}
+		x = readInt(bs, x, &res[i])
+	}
+	return res
+}
+
+func solve(n int, p []int) int {
+	g := NewGraph(n, n)
+	for i := 1; i < n; i++ {
+		g.AddEdge(p[i-1]-1, i)
+	}
+	dep := make([]int, n)
+	var dfs func(u int)
+
+	dfs = func(u int) {
+		for i := g.nodes[u]; i > 0; i = g.next[i] {
+			v := g.to[i]
+			dep[v] = dep[u] + 1
+			dfs(v)
+		}
+	}
+
+	dfs(0)
+	cnt := make([]int, n)
+	for i := 0; i < n; i++ {
+		cnt[dep[i]]++
+	}
+
+	var res int
+	for i := 0; i < n; i++ {
+		res += cnt[i] & 1
+	}
+
+	return res
+}
+
+type Graph struct {
+	nodes []int
+	next  []int
+	to    []int
+	cur   int
+}
+
+func NewGraph(n int, e int) *Graph {
+	nodes := make([]int, n)
+	next := make([]int, e)
+	to := make([]int, e)
+	return &Graph{nodes, next, to, 0}
+}
+
+func (g *Graph) AddEdge(u, v int) {
+	g.cur++
+	g.next[g.cur] = g.nodes[u]
+	g.nodes[u] = g.cur
+	g.to[g.cur] = v
+}

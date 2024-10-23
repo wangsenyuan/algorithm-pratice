2d9272da399c0b18d0faf031eda4bc61705617d0
WangSenyuan <brucewang@maycur.com>
2024-10-23 21:31:43 +0800

    finally find the problem

--- src/codeforces/set1/set12/set124/set1242/c/solution.go
+++ src/codeforces/set1/set12/set124/set1242/c/solution.go
@@ -122,6 +122,144 @@ func solve(boxes [][]int) [][]int {
 		}
 	}
 
+	N := 1 << n
+	dp := make([]bool, N)
+
+	type pair struct {
+		first  int
+		second int
+	}
+
+	from := make([]pair, N)
+	for i := 0; i < N; i++ {
+		from[i] = pair{-1, -3}
+	}
+
+	var dfs func(p int, u int, x int, flag int) int
+
+	dfs = func(p int, u int, x int, flag int) int {
+		if u == p {
+			return flag
+		}
+		if flag&(1<<u) > 0 {
+			// 回到了前面的节点，但是不是开始位置
+			return -1
+		}
+		// 这里的y是确定的
+		// box - x + y = sum
+		y := sum - box_sum[u] + x
+		if v, ok := comp[y]; ok {
+			return dfs(p, v, y, flag|(1<<u))
+		}
+		return -1
+	}
+	// 这里的复杂度是 n * n * 5000
+	for i := 0; i < n; i++ {
+		for _, x := range boxes[i] {
+			// 需要清理状态
+			ii := i
+			flag := 1 << i
+			val := x
+			for !dp[flag] {
+				y := sum - box_sum[ii] + val
+				if y == x {
+					dp[flag] = true
+					from[flag] = pair{x, i}
+					break
+				}
+				if j, ok := comp[y]; !ok || (flag>>j)&1 == 1 {
+					break
+				} else {
+					flag |= 1 << j
+					ii = j
+					val = y
+				}
+			}
+		}
+	}
+
+	for state := 1; state < N; state++ {
+		if dp[state] {
+			// already good
+			continue
+		}
+		for tmp := state; tmp > 0; tmp = (tmp - 1) & state {
+			// 要找到它的子集看看，能不能完成
+			if dp[tmp] && dp[state^tmp] {
+				dp[state] = true
+				from[state] = pair{tmp, -1}
+				break
+			}
+		}
+	}
+
+	if !dp[N-1] {
+		return nil
+	}
+
+	res := make([][]int, n)
+
+	var construct func(state int)
+
+	var visit func(u int, x int, flag int)
+
+	construct = func(state int) {
+		if from[state].second == -1 {
+			a := from[state].first
+			b := state ^ a
+			construct(a)
+			construct(b)
+			return
+		}
+		if state&(state-1) == 0 {
+			u := from[state].second
+			x := boxes[u][0]
+			res[u] = []int{x, u + 1}
+			return
+		}
+		x, u := from[state].first, from[state].second
+		visit(u, x, 0)
+	}
+
+	visit = func(u int, x int, flag int) {
+		if flag&(1<<u) > 0 {
+			return
+		}
+		y := sum - (box_sum[u] - x)
+		v := comp[y]
+		res[v] = []int{y, u + 1}
+		visit(v, y, flag|(1<<u))
+	}
+
+	construct(N - 1)
+
+	return res
+}
+
+func solve1(boxes [][]int) [][]int {
+	n := len(boxes)
+	var sum int
+
+	box_sum := make([]int, n)
+
+	for i, box := range boxes {
+		for _, num := range box {
+			sum += num
+			box_sum[i] += num
+		}
+	}
+	if sum%n != 0 {
+		return nil
+	}
+	sum /= n
+
+	comp := make(map[int]int)
+	for i, box := range boxes {
+		for _, num := range box {
+			comp[num] = i
+		}
+	}
+
 	give := make([]int, n)
 	to := make([]int, n)
 	vis := make([]bool, 1<<n)
